ea250cdd086059ea7976a38c8e94cb4a39709357
Matthias Kestenholz <mk@spinlock.ch>
2009-03-02 17:56:40 +0100

    Call the setup_request page method too in generic views replacements

--- feincms/views/decorators.py
+++ feincms/views/decorators.py
@@ -9,7 +9,7 @@ from feincms.models import Page
 def add_page_to_extra_context(view_func):
     def inner(request, *args, **kwargs):
         kwargs.setdefault('extra_context', {})
-        kwargs['extra_context']['feincms_page'] = Page.objects.best_match_for_path(request.path)
+        kwargs['extra_context']['feincms_page'] = Page.objects.best_match_from_request(request)
 
         return view_func(request, *args, **kwargs)
     return wraps(view_func)(inner)
