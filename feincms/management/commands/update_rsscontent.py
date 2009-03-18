a1bfc9a83341960ee022135d5560d21ba82b48e9
Matthias Kestenholz <mk@spinlock.ch>
2009-03-18 15:09:25 +0100

    Add update_rsscontent management command

---
+++ feincms/management/commands/update_rsscontent.py
@@ -0,0 +1,12 @@
+from django.core.management.base import NoArgsCommand
+
+from feincms.content.rss.models import RSSContent
+
+class Command(NoArgsCommand):
+    help = "Run this as a cronjob."
+
+    def handle_noargs(self, **options):
+        for cls in RSSContent._feincms_content_models:
+            for content in cls.objects.all():
+                content.cache_content()
+
