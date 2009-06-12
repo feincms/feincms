0c48bc378266122f7f3f4c3651cb1dd81fe84e81
Matthias Kestenholz <mk@spinlock.ch>
2009-06-12 15:37:35 +0200

    Add RawContent content type for raw code output

---
+++ feincms/content/raw/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
+
+
+class RawContent(models.Model):
+    text = models.TextField(_('text'), blank=True)
+
+    class Meta:
+        abstract = True
+        verbose_name = _('raw content')
+        verbose_name_plural = _('raw contents')
+
+    def render(self, **kwargs):
+        return mark_safe(self.text)
+
