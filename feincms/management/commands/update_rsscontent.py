58078b1d4eb64c7104715352fc11bf7abffd48a4
Matthias Kestenholz <mk@spinlock.ch>
2009-06-16 00:47:53 +0200

    Add small explaining note to the RSSContent updating management command

--- feincms/management/commands/update_rsscontent.py
+++ feincms/management/commands/update_rsscontent.py
@@ -6,6 +6,7 @@ class Command(NoArgsCommand):
     help = "Run this as a cronjob."
 
     def handle_noargs(self, **options):
+        # find all concrete content types of RSSContent
         for cls in RSSContent._feincms_content_models:
             for content in cls.objects.all():
                 content.cache_content()
