40dae2a4b5b6bfaaa84784ac6a6621af1d0d893b
Matthias Kestenholz <mk@spinlock.ch>
2009-06-16 22:58:33 +0200

    Refactor VideoContent -- uses templates and works with vimeo too now

--- feincms/content/video/models.py
+++ feincms/content/video/models.py
@@ -1,10 +1,19 @@
-from django.db import models
-from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_lazy as _
 import re
 
+from django.db import models
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
+
+
 class VideoContent(models.Model):
-    video = models.URLField(_('video link'),help_text=_('This should be a link to a youtube video, i.e.: http://www.youtube.com/watch?v=zmj1rpzDRZ0'))
+    PORTALS = (
+        ('youtube', re.compile('youtube'), lambda url: {'v': re.search(r'[?&]v=(\w+)', url).group(1)}),
+        ('vimeo', re.compile('vimeo'), lambda url: {'id': re.search(r'/(\d)+', url).group(1)}),
+        )
+
+    video = models.URLField(_('video link'),
+        help_text=_('This should be a link to a youtube or vimeo video, i.e.: http://www.youtube.com/watch?v=zmj1rpzDRZ0'))
 
     class Meta:
         abstract = True
@@ -12,15 +21,12 @@ class VideoContent(models.Model):
         verbose_name_plural = _('videos')
 
     def render(self, **kwargs):
-        vid = re.search('(?<==)\w+',self.video)
-        ret = """
-            <div class="videocontent">
-            <object width="400" height="330">
-            <param name="movie" value="http://www.youtube.com/v/%s&hl=de&fs=1"></param>
-                <param name="allowFullScreen" value="true"></param>
-                <param name="allowscriptaccess" value="always"></param>
-                <embed src="http://www.youtube.com/v/%s&hl=de&fs=1&rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="400" height="330"></embed>
-            </object>
-            </div>
-            """ % (vid.group(0), vid.group(0))
-        return ret
+        for portal, match, context_fn in self.TYPES:
+            if match.search(self.video):
+                return render_to_string([
+                    'content/video/%s.html' % portal,
+                    'content/video/unknown.html',
+                    ], dict(context_fn(self.video), content=self))
+
+        return render_to_string('content/video/unknown.html', {
+            'content': self})
