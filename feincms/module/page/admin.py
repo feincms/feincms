5025175c474722362e82716861dc8ba4f79bfff4
Matthias Kestenholz <mk@spinlock.ch>
2009-06-25 14:43:21 +0200

    The Page model needs at least one registered template

--- feincms/module/page/admin.py
+++ feincms/module/page/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 
 
 from feincms.admin import editor
 from feincms.module.page.models import Page
+
+
+if not hasattr(Page, 'template'):
+    raise ImproperlyConfigured, 'You need to register at least one template for Page before the admin code is includede'
 
 
 class PageAdmin(editor.ItemEditorMixin, editor.TreeEditorMixin, admin.ModelAdmin):
