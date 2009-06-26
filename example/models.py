496865dccb410acf905aaa57311cf036929b9a98
Matthias Kestenholz <mk@spinlock.ch>
2009-06-26 12:17:38 +0200

    Update example to work again

--- example/models.py
+++ example/models.py
@@ -2,5 +2,14 @@ from feincms.module.page.models import Page
 from feincms.content.raw.models import RawContent
 from feincms.content.image.models import ImageContent
 
+Page.register_templates({
+    'key': 'base',
+    'title': 'Base Template',
+    'path': 'base.html',
+    'regions': (
+        ('main', 'Main region'),
+        ('sidebar', 'Sidebar', 'inherited'),
+        ),
+    })
 Page.create_content_type(RawContent)
 Page.create_content_type(ImageContent)
