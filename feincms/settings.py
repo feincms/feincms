1c1a9b38e7926c981769ff71c0c18a3719ebaa7f
Matthias Kestenholz <mk@spinlock.ch>
2009-07-08 10:13:17 +0200

    FEINCMS_MEDIALIBRARY_PATH should contain an absolute filesystem path
    It is passed to FileSsystemStorage(location=...).
    
    Furthermore, reduce deviations from default Django media handling where
    possible.

--- feincms/settings.py
+++ feincms/settings.py
@@ -11,11 +11,11 @@ from os.path import join
 # Settings for MediaLibrary
 
 # Local path to uploaded media files
-FEINCMS_MEDIALIBRARY_PATH = getattr(settings, 'FEINCMS_MEDIALIBRARY_PATH', 'medialibrary/')
+FEINCMS_MEDIALIBRARY_PATH = getattr(settings, 'FEINCMS_MEDIALIBRARY_PATH', settings.MEDIA_ROOT)
 # Local path to newly uploaded media files
-FEINCMS_MEDIALIBRARY_FILES = getattr(settings, 'FEINCMS_MEDIALIBRARY_FILES', FEINCMS_MEDIALIBRARY_PATH + '%Y/%m/')
+FEINCMS_MEDIALIBRARY_FILES = getattr(settings, 'FEINCMS_MEDIALIBRARY_FILES', 'medialibrary/%Y/%m/')
 # URL to access media library files
-FEINCMS_MEDIALIBRARY_URL  = getattr(settings, 'FEINCMS_MEDIALIBRARY_URL', '/medialibrary/')
+FEINCMS_MEDIALIBRARY_URL  = getattr(settings, 'FEINCMS_MEDIALIBRARY_URL', settings.MEDIA_URL)
 
 # ------------------------------------------------------------------------
 # ------------------------------------------------------------------------
