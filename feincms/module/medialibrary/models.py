feincms/feincms

commit 8af7915086c3c5e664261d353a4ad5dc33eddf3d

Trim superfluous properties.

feincms/module/medialibrary/models.py
@@ -55,14 +55,6 @@ class MediaFile(models.Model, TranslatedObjectMixin):
             if callable(upload_to):
                 f.generate_filename = upload_to
 
-    @property
-    def file_name(self):
-        return self.file.name
-
-    @property
-    def file_url(self):
-        return self.file.url
-
     def get_absolute_url(self):
         return self.file_url
 

commit 6a943af0fa3214c91ab8dfa005e12063d944e385

Correct bug introduced in 8af7915086c3c5e664261d353a4ad5dc33eddf3d

feincms/module/medialibrary/models.py
@@ -76,7 +76,7 @@ class MediaFile(models.Model, TranslatedObjectMixin):
                 f.generate_filename = upload_to
 
     def get_absolute_url(self):
-        return self.file_url
+        return self.file.url
 
     def file_type(self):
         return self.FILE_TYPES_DICT[self.type]