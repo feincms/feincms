836f6e409642f93358011d0c10064cf04da0ab63
Matthias Kestenholz <mk@spinlock.ch>
2009-07-16 21:43:24 +0200

    The URL pattern should match the root page too (empty string)

--- feincms/urls.py
+++ feincms/urls.py
@@ -4,5 +4,5 @@ from feincms.views import ajax, base
 
 urlpatterns = patterns('',
     url(r'^preview/(?P<page_id>\d+)/', base.preview_handler, name='feincms:preview'),
-    url(r'^(.*)/$', base.handler),
+    url(r'^$|^(.*)/$', base.handler), # catch empty URLs (root page) or URLs ending with a slash
 )
