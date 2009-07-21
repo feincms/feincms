d636cce33b993eac5350fbe4b227fd797a0fad60
mjl <mjl@laubach.at>
2009-07-21 10:07:07 +0200

    Instead of monkey patching Page.save(), use the pre_save signal instead.

--- feincms/module/page/extensions/changedate.py
+++ feincms/module/page/extensions/changedate.py
@@ -8,18 +8,22 @@ from django.db import models
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.db.models.signals import pre_save
 
+from feincms.module.page.models import Page
+
+def pre_save_handler(sender, instance, **kwargs):
+    """
+    Intercept attempts to save and insert the current date and time into
+    creation and modification date fields.
+    """
+    now = datetime.now()
+    if instance.id is None:
+        instance.creation_date = now
+    instance.modification_date = now
 
 def register(cls, admin_cls):
-    cls.add_to_class('creation_date', models.DateTimeField(_('creation date'), editable=False))
+    cls.add_to_class('creation_date',     models.DateTimeField(_('creation date'),     editable=False))
     cls.add_to_class('modification_date', models.DateTimeField(_('modification date'), editable=False))
 
-    orig_save = cls.save
-    def save(page):
-        now = datetime.now()
-        if page.id is None:
-            page.creation_date = now
-        page.modification_date = now
-        orig_save(page)
-
-    cls.save = save
+    pre_save.connect(pre_save_handler, sender=cls)
