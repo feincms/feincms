28589ba759c958f23b7c47f3c06a4932f6b3c5e8
Matthias Kestenholz <mk@spinlock.ch>
2009-07-25 18:29:09 +0200

    Make SplitPaneEditor sort-of usable
    The structure still cannot be updated.

--- feincms/admin/splitpane_editor.py
+++ feincms/admin/splitpane_editor.py
@@ -25,8 +25,15 @@ class SplitPaneEditor(admin.ModelAdmin):
         if request.is_ajax():
             return HttpResponse('hello world')
 
-        if 'tree' in request.GET:
+        if '_tree' in request.GET:
             return self._tree_view(request)
+
+        if '_blank' in request.GET:
+            return self._blank_view(request)
+
+        if 'pop' in request.GET:
+            # Delegate to default implementation for raw_id_fields etc
+            return super(SplitPaneEditor, self).changelist_view(request, extra_context)
 
         return render_to_response('admin/feincms/splitpane_editor.html')
 
@@ -34,4 +41,16 @@ class SplitPaneEditor(admin.ModelAdmin):
         return render_to_response('admin/feincms/splitpane_editor_tree.html', {
             'object_list': self.model._tree_manager.all(),
             'FEINCMS_ADMIN_MEDIA': settings.FEINCMS_ADMIN_MEDIA,
+            }, context_instance=template.RequestContext(request))
+
+    def _blank_view(self, request):
+        from django.contrib.admin.views.main import ChangeList, ERROR_FLAG
+        opts = self.model._meta
+        app_label = opts.app_label
+
+        return render_to_response('admin/feincms/splitpane_editor_blank.html', {
+            'title': opts.verbose_name_plural,
+            'has_add_permission': self.has_add_permission(request),
+            'root_path': self.admin_site.root_path,
+            'opts': opts,
             }, context_instance=template.RequestContext(request))
