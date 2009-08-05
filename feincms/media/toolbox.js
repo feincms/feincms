1f0a7d2e5b4050d3bf387fa568c3583c9a023889
mjl <mjl@laubach.at>
2009-08-06 00:58:02 +0200

    Move JS utilities from inline in page/change_list.html to own generic .js file.

---
+++ feincms/media/toolbox.js
@@ -0,0 +1,44 @@
+/* Contains universally useful functions */
+
+/* Extract an object id (numeric) from a DOM id. Assumes that a "-" is used
+   as delimiter. Returns either the id found or 0 if something went wrong.
+
+       extract_item_id('foo_bar_baz-327') -> 327
+ */
+var extract_item_id = function(elem_id) {
+    i = elem_id.indexOf('-');
+    if(i >= 0)
+        return parseInt(elem_id.slice(i+1));
+    
+    return 0;
+}
+
+/* Given an html snippet (in text form), parses it to extract the id attribute,
+   then replace the corresponding element in the page with the snippet. The
+   first parameter is ignored (so the signature matches what $.each expects).
+ 
+       replace_element(0, '<div id="replace_me">New Stuff!</div>')
+ */
+var replace_element = function(i, html) {
+    r_id = $(html).attr('id');
+    $('#' + r_id).replaceWith(html);
+}
+
+/* Same as above, but processes an array of html snippets */
+var replace_elements = function(data) {
+    $.each(data, replace_element);
+}
+
+/* OnClick handler to toggle a boolean field via AJAX */
+var inplace_toggle_boolean = function(elem, attr) {
+    elem_id = $(elem).attr('id');
+    item_id = extract_item_id(elem_id);
+    $.post('.', {
+           '__cmd': 'toggle_boolean',
+           'item_id': item_id,
+           'attr': attr
+           },
+           replace_elements,
+           'json');
+    return false;
+}
