ce6600131dc4c1ff2591723bcebfad1904c40ca6
mjl <mjl@laubach.at>
2009-08-11 14:04:04 +0200

    Add "parent" as filter and use JS to sieve out leaf entries. This gives a quick site structure for navigation.

--- feincms/module/page/models.py
+++ feincms/module/page/models.py
@@ -498,7 +498,7 @@ class PageAdmin(editor.ItemEditor, list_modeladmin):
         }),
         )
     list_display = ['short_title', 'is_visible_admin', 'in_navigation_toggle', 'template']
-    list_filter = ('active', 'in_navigation', 'template_key')
+    list_filter = ('active', 'in_navigation', 'template_key', 'parent')
     search_fields = ('title', 'slug')
     prepopulated_fields = { 'slug': ('title',), }
 
