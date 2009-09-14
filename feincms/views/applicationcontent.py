7ac257514fd3058565bf31d69a7c87d005448518
mjl <mjl@laubach.at>
2009-09-14 23:59:40 +0200

    Cache a has_appcontent indication to skip fetching and processing non-existant app contents for most pages.
    The statistics geeks says test case page is now :
    
      *  4 queries, 0.022 ms

--- feincms/views/applicationcontent.py
+++ feincms/views/applicationcontent.py
@@ -1,9 +1,16 @@
+# ------------------------------------------------------------------------
+# coding=utf-8
+# ------------------------------------------------------------------------
+
 from django.http import Http404
 
+from feincms import settings
 from feincms.content.application.models import retrieve_page_information
 from feincms.module.page.models import Page
 from feincms.views.base import _build_page_response
 
+# ------------------------------------------------------------------------
+# TODO: This routine is very similar to the one in base.py, perhaps we should look into unifying them?
 
 def handler(request, path=None):
     if path is None:
@@ -11,13 +18,26 @@ def handler(request, path=None):
 
     page = Page.objects.best_match_for_path(path, raise404=True)
 
-    applicationcontents = page.applicationcontent_set.all()
+    from django.core.cache import cache as django_cache
+
+    # Try to avoid the lookup of app contents by caching, since nodes
+    # with app content are a rather rare occurrence, this is a win in
+    # most cases.
+    has_appcontent = True
+    if settings.FEINCMS_USE_CACHE:
+        ck = 'HAS-APP-CONTENT-' + page.cache_key()
+        has_appcontent = django_cache.get(ck)
+
+    if has_appcontent:
+        applicationcontents = page.applicationcontent_set.all()
+        has_appcontent = any(applicationcontents)
+        if settings.FEINCMS_USE_CACHE:
+            django_cache.set(ck, has_appcontent)
 
     if request.path != page.get_absolute_url():
         # The best_match logic kicked in. See if we have at least one
         # application content for this page, and raise a 404 otherwise.
-
-        if not applicationcontents:
+        if not has_appcontent:
             raise Http404
 
     # The monkey-patched reverse() method needs some information
@@ -29,11 +49,14 @@ def handler(request, path=None):
     if response:
         return response
 
-    for content in applicationcontents:
-        r = content.process(request)
-        if r and (r.status_code != 200 or request.is_ajax() or getattr(r, 'standalone', False)):
-            return r
+    if has_appcontent:
+        for content in applicationcontents:
+            r = content.process(request)
+            if r and (r.status_code != 200 or request.is_ajax() or getattr(r, 'standalone', False)):
+                return r
 
     response = _build_page_response(page, request)
     page.finalize_response(request, response)
     return response
+
+# ------------------------------------------------------------------------
