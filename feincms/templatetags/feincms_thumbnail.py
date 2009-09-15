cdd3f563d7aa7ef7fb70ad4a5f3b0d00fe6b6faa
Matthias Kestenholz <mk@spinlock.ch>
2009-09-15 18:21:43 +0200

    Add thumbnail library
    It is very stupid, but does what it should most of the time.

---
+++ feincms/templatetags/feincms_thumbnail.py
@@ -0,0 +1,62 @@
+from random import random
+import os, Image
+
+from django import template
+from django.conf import settings
+
+
+register = template.Library()
+
+@register.filter
+def thumbnail(filename, size='200x200'):
+    # defining the size
+    x, y = [int(x) for x in size.split('x')]
+    # defining the filename and the miniature filename
+    basename, format = filename.rsplit('.', 1)
+    miniature = basename + '_thumb_' + size + '.' +  format
+    miniature_filename = os.path.join(settings.MEDIA_ROOT, miniature)
+    miniature_url = os.path.join(settings.MEDIA_URL, miniature)
+    orig_filename = os.path.join(settings.MEDIA_ROOT, filename)
+    # if the image wasn't already resized, resize it
+    if not os.path.exists(miniature_filename) or (os.path.getmtime(miniature_filename)<os.path.getmtime(orig_filename)):
+        image = Image.open(orig_filename)
+        image.thumbnail([x, y], Image.ANTIALIAS) # generate a 200x200 thumbnail
+        image.save(miniature_filename, image.format)
+    return miniature_url
+
+
+@register.filter
+def cropscale(filename, size='200x200'):
+    w, h = [int(x) for x in size.split('x')]
+
+    basename, format = filename.rsplit('.', 1)
+    miniature = basename + '_cropscale_' + size + '.' +  format
+    miniature_filename = os.path.join(settings.MEDIA_ROOT, miniature)
+    miniature_url = os.path.join(settings.MEDIA_URL, miniature)
+    orig_filename = os.path.join(settings.MEDIA_ROOT, filename)
+    # if the image wasn't already resized, resize it
+    if not os.path.exists(miniature_filename) or (os.path.getmtime(miniature_filename)<os.path.getmtime(orig_filename)):
+        image = Image.open(orig_filename)
+
+        src_width, src_height = image.size
+        src_ratio = float(src_width) / float(src_height)
+        dst_width, dst_height = w, h
+        dst_ratio = float(dst_width) / float(dst_height)
+
+        if dst_ratio < src_ratio:
+            crop_height = src_height
+            crop_width = crop_height * dst_ratio
+            x_offset = float(src_width - crop_width) / 2
+            y_offset = 0
+        else:
+            crop_width = src_width
+            crop_height = crop_width / dst_ratio
+            x_offset = 0
+            y_offset = float(src_height - crop_height) / 2
+
+        image = image.crop((x_offset, y_offset, x_offset+int(crop_width), y_offset+int(crop_height)))
+        image = image.resize((dst_width, dst_height), Image.ANTIALIAS)
+        image.save(miniature_filename, image.format)
+
+    return miniature_url
+
