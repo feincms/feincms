f3dd4921ee3904af7bd55c3e776e15a05a11a25d
Chris Adams <chris@improbable.org>
2009-09-29 18:10:12 -0400

    Custom admin filterspec for list_filter parent
    This filter indents the list_filter for parent correctly, avoiding the
    need for us to indent the Page.__unicode__ output.

---
+++ feincms/admin/filterspecs.py
@@ -0,0 +1,52 @@
+# encoding=utf-8
+# Thanks to http://www.djangosnippets.org/snippets/1051/
+#
+# Authors: Marinho Brandao <marinho at gmail.com>
+#          Guilherme M. Gondim (semente) <semente at taurinus.org>
+
+
+from django.contrib.admin.filterspecs import FilterSpec, ChoicesFilterSpec
+from django.utils.encoding import smart_unicode
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+
+class ParentFilterSpec(ChoicesFilterSpec):
+    """
+    Improved list_filter display for parent Pages by nicely indenting hierarchy
+    
+    In theory this would work with any mptt model which uses a "title" attribute.
+
+    my_model_field.page_parent_filter = True
+    """
+
+    def __init__(self, f, request, params, model, model_admin):
+        import logging
+        super(ParentFilterSpec, self).__init__(f, request, params, model, model_admin)
+
+        parent_ids = model.tree.exclude(parent=None).values_list("parent__id", flat=True).order_by("parent__id").distinct()
+        parents = model.tree.filter(pk__in=parent_ids).values_list("pk", "title", "level")
+        self.lookup_choices = [(pk, "%s%s" % ("&nbsp;" * level, title)) for pk, title, level in parents]
+
+        logging.info("ParentFilter initialized with %d choices" % len(self.lookup_choices))
+
+    def choices(self, cl):
+        yield { 
+            'selected':     self.lookup_val is None,
+            'query_string': cl.get_query_string({}, [self.lookup_kwarg]),
+            'display':      _('All')
+        }
+        
+        for pk, title in self.lookup_choices:
+            yield {
+                'selected':     pk == self.lookup_val,
+                'query_string': cl.get_query_string({self.lookup_kwarg: pk}),
+                'display':      mark_safe(smart_unicode(title))
+            }
+            
+    def title(self):
+        return _('Parent')
+
+# registering the filter
+FilterSpec.filter_specs.insert(0, 
+    (lambda f: getattr(f, 'parent_filter', False), ParentFilterSpec)
+)
