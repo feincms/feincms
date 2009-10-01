b6dadb962591a9770866b446d5727fd7262a480a
mjl <mjl@laubach.at>
2009-10-01 11:38:05 +0200

    Drop the ugly hack prepending nbsp to unicode representation of Page in favor of custom filter specs.

--- feincms/module/page/models.py
+++ feincms/module/page/models.py
@@ -212,10 +212,7 @@ class Page(Base):
     objects = PageManager()
 
     def __unicode__(self):
-        # Note: These are actually two "no-break spaces". They are here so that the
-        # "filter by parent" drop down shows a hierarchical outline.
-        return mark_safe(u'  ' * self.level + self.short_title())
-        # return self.short_title()
+        return self.short_title()
 
     def is_active(self):
         """
