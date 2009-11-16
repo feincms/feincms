fd4484cfa3be83a904d955bc7af5bf78571663c8
Chris Adams <chris@improbable.org>
2009-11-16 10:21:58 -0500

    Fixed seo extension reliability
    It used to have some hard-coded assumptions about the model it was being
    registered for. Now it conditionally updates the admin fieldsets only if
    it finds the appropriate fieldset

--- feincms/module/extensions/seo.py
+++ feincms/module/extensions/seo.py
@@ -2,9 +2,10 @@
 Adds several fields which are helpful for SEO optimization
 """
 
+import logging
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-
 
 def register(cls, admin_cls):
     cls.add_to_class('meta_keywords', models.TextField(_('meta keywords'), blank=True,
@@ -13,5 +14,11 @@ def register(cls, admin_cls):
         help_text=_('This will be prepended to the default description.')))
 
     if admin_cls:
-        admin_cls.fieldsets[1][1]['fields'].extend(['meta_keywords', 'meta_description'])
-        admin_cls.search_fields.extend(['meta_keywords', 'meta_description'])
+        admin_cls.search_fields += ('meta_keywords', 'meta_description')
+
+        if admin_cls.fieldsets:
+            fieldsets = [ f for f in admin_cls.fieldsets if f[0] == "Other options"]
+            if fieldsets:
+                fieldsets[0][1]['fields'].extend(['meta_keywords', 'meta_description'])
+            else:
+                logging.warning("Couldn't determine which fieldset on %s should have the seo fields", admin_cls)
