6a5af4d26430fa9365281a5f5850a6b39aa34341
Matthias Kestenholz <mk@spinlock.ch>
2009-12-10 10:52:04 +0100

    Stop using Django settings at module level

--- feincms/__init__.py
+++ feincms/__init__.py
@@ -1,2 +1,19 @@
 VERSION = (1, 0, 4)
 __version__ = '.'.join(map(str, VERSION))
+
+
+# Do not use Django settings at module level as recommended
+from django.utils.functional import LazyObject
+from feincms import default_settings
+
+class LazySettings(LazyObject):
+    def _setup(self):
+        self._wrapped = Settings(default_settings)
+
+class Settings(object):
+    def __init__(self, settings_module):
+        for setting in dir(settings_module):
+            if setting == setting.upper():
+                setattr(self, setting, getattr(settings_module, setting))
+
+settings = LazySettings()
