84544515fd4ea273bfbdf7b983ff6b623f814e14
Matthias Kestenholz <mk@spinlock.ch>
2009-12-16 12:15:12 +0100

    Fix issue #39: Setup was broken
    Thanks wernight.

--- feincms/__init__.py
+++ feincms/__init__.py
@@ -4,10 +4,10 @@ __version__ = '.'.join(map(str, VERSION))
 
 # Do not use Django settings at module level as recommended
 from django.utils.functional import LazyObject
-from feincms import default_settings
 
 class LazySettings(LazyObject):
     def _setup(self):
+        from feincms import default_settings
         self._wrapped = Settings(default_settings)
 
 class Settings(object):
