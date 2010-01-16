8ee5ef7bc9e8c564ad197f3fbf04e3abd71bfe55
mjl <mjl@laubach.at>
2010-01-16 11:53:30 +0100

    Simplify the ct_tracker enhancement in d88a08f1: Use a JSONField instead of manually managing the encoding/decoding; keep the "has appcontent" flag directly in the inventory; remove unused code.

--- feincms/module/page/extensions/ct_tracker.py
+++ feincms/module/page/extensions/ct_tracker.py
@@ -19,10 +19,11 @@ saving time, thus saving a db query on page delivery.
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models.signals import pre_save
-from django.utils import simplejson
 from django.utils.translation import ugettext_lazy as _
 
 # ------------------------------------------------------------------------
+HAS_APPCONTENT_KEY = '_has_appcontent'
+
 def page_count_content_types(self):
     """
     Returns a representation of all the content types present on a page.
@@ -48,13 +49,24 @@ def page_count_content_types(self):
 
         # Now convert the content types to django ContentType.id, so the result
         # set is stable wrt. registered feincms content types.
+        has_appcontent = False
         for ct_idx, region, count in row:
             from django.contrib.contenttypes.models import ContentType
+            from feincms.content.application.models import ApplicationContent
 
             if count:
                 if not ct_inventory.has_key(region):
-                    ct_inventory[region] = []
-                ct_inventory[region].append(ContentType.objects.get_for_model(self._feincms_content_types[ct_idx]).id)
+                    ct_inventory[region] = list()
+
+                feincms_ct = self._feincms_content_types[ct_idx]
+                django_ct = ContentType.objects.get_for_model(feincms_ct)
+
+                ct_inventory[region].append(django_ct.id)
+
+                if issubclass(feincms_ct, ApplicationContent):
+                    has_appcontent = True
+
+        ct_inventory[HAS_APPCONTENT_KEY] = has_appcontent
 
     return ct_inventory
 
@@ -74,13 +86,6 @@ def get_tr_map(self):
             tr_map[ContentType.objects.get_for_model(ct).id] = idx
         setattr(self.__class__, '_django_ct_to_feincms_ct_map', tr_map)
 
-        # ???: How likely there is more than one app content? Do we really need a list here?
-        from feincms.content.application.models import ApplicationContent
-        app_ct_list = [ x.id for x in
-                    (ContentType.objects.get_for_model(t) for t in self._feincms_content_types
-                                    if issubclass(t, ApplicationContent)) ]
-        setattr(self.__class__, '_django_app_ct_list', app_ct_list)
-
     return tr_map
 
 # ------------------------------------------------------------------------
@@ -88,10 +93,9 @@ def page_get_content_types_for_region(self, region):
     """
     Overrides Page.get_content_types_for_region.
     """
-    inv = simplejson.loads(self._ct_inventory)
-
+    inv = self._ct_inventory
     retval = [0] * len(self._feincms_content_types)
-    region_ct_inventory = inv.get(region.key, [])
+    region_ct_inventory = inv.get(region.key, ())
 
     tr_map = get_tr_map(self)
 
@@ -101,18 +105,22 @@ def page_get_content_types_for_region(self, region):
     return retval
 
 # ------------------------------------------------------------------------
+def has_appcontent(self):
+    inv = self._ct_inventory
+    return inv.get(HAS_APPCONTENT_KEY, False)
 
 # ------------------------------------------------------------------------
 def pre_save_handler(sender, instance, **kwargs):
     """
     Intercept save and store the currently used content types into the page itself.
     """
-    ct_inventory = instance.count_content_types()
-    instance._ct_inventory = simplejson.dumps(ct_inventory)
+    instance._ct_inventory = instance.count_content_types()
 
 # ------------------------------------------------------------------------
 def register(cls, admin_cls):
-    cls.add_to_class('_ct_inventory', models.CharField(_('content types'), max_length=500, editable=False))
+    from feincms.contrib.fields import JSONField
+
+    cls.add_to_class('_ct_inventory', JSONField(_('content types'), editable=False, blank=True, null=True))
     cls.add_to_class('count_content_types', page_count_content_types)
 
     cls.orig_get_content_types_for_region = cls._get_content_types_for_region
@@ -121,21 +129,7 @@ def register(cls, admin_cls):
     pre_save.connect(pre_save_handler, sender=cls)
 
     # Optimize views.applicationcontent since we know what ct are in this page
-    from feincms.content.application.models import ApplicationContent
-    from django.contrib.contenttypes.models import ContentType
-
-    def has_appcontent(self):
-        get_tr_map(self)    # prime app_ct_list
-
-        app_ct_list = getattr(self.__class__, '_django_app_ct_list', [])
-
-        inv = simplejson.loads(self._ct_inventory)
-        for k in inv.itervalues():
-            for ct in app_ct_list:
-                if ct in k:
-                    return True
-        return False
-
     import feincms.views.applicationcontent
     feincms.views.applicationcontent.page_has_appcontent = has_appcontent
+
 # ------------------------------------------------------------------------
