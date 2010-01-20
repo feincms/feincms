efb7191428756f8ef0b85475d00297e2594eca4c
mjl <mjl@laubach.at>
2010-01-20 19:13:25 +0100

    Handle posting in comments content type so the "post comment" stays in the cms framework instead of showing an external page from django.contrib.comments.

--- feincms/content/comments/models.py
+++ feincms/content/comments/models.py
@@ -26,11 +26,26 @@ class CommentsContent(models.Model):
         parent_type = self.parent.__class__.__name__.lower()
         request = kwargs.get('request')
 
+        # TODO: Check for translation extension before use!
+        comment_page = self.parent.original_translation
+
+        f = None
+        if self.comments_enabled and request.POST:
+            extra = request._feincms_appcontent_parameters.get('page_extra_path', ())
+            if len(extra) > 0 and extra[0] == u"post-comment":
+                from django.contrib.comments.views.comments import post_comment
+                r = post_comment(request)
+                if not isinstance(r, HttpResponseRedirect):
+                    f = comments.get_form()(comment_page, data=request.POST)
+
+        if f is None:
+            f = comments.get_form()(comment_page)
+
         return render_to_string([
             'content/comments/%s.html' % parent_type,
             'content/comments/default-site.html',
             'content/comments/default.html',
-            ], RequestContext(request, { 'content': self, 'parent': self.parent }))
+            ], RequestContext(request, { 'content': self, 'feincms_page' : self.parent, 'parent': comment_page, 'form' : f }))
 
 # ------------------------------------------------------------------------
 
