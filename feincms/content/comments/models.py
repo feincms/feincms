2ba05098195a8bdced1c602969f98fa7eb022fc9
mjl <mjl@laubach.at>
2010-01-20 19:15:04 +0100

    Sort imports. Add comments for ItemEditor changes.

--- feincms/content/comments/models.py
+++ feincms/content/comments/models.py
@@ -6,21 +6,39 @@
 #
 # ------------------------------------------------------------------------
 
-
+from django import forms
+from django.contrib import comments
+from django.contrib.comments.models import Comment
 from django.db import models
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
-from django.template.loader import render_to_string
-from django.template import RequestContext
+
+# from feincms.admin.editor import ItemEditorForm
 
 # ------------------------------------------------------------------------
 class CommentsContent(models.Model):
-    comments_enabled = models.BooleanField(_('enabled'), default=True)
+    comments_enabled = models.BooleanField(_('enabled'), default=True, help_text=_('New comments may be added'))
 
     class Meta:
         abstract = True
         verbose_name = _('comments')
         verbose_name_plural = _('comments')
+
+ #   @classmethod
+ #   def initialize_type(cls):
+ #       class CommentContentAdminForm(ItemEditorForm):
+ #           def __init__(self, *args, **kwargs):
+ #               parent = kwargs.get('instance', None)
+ #               if parent is not None:
+ #                   for c in Comment.objects.order_by('submit_date'):
+ #                       self.base_fields['comment_%d' % c.id] = forms.BooleanField('comment')
+ #               super(CommentContentAdminForm, self).__init__(*args, **kwargs)
+
+
+ #       cls.feincms_item_editor_form = CommentContentAdminForm
 
     def render(self, **kwargs):
         parent_type = self.parent.__class__.__name__.lower()
