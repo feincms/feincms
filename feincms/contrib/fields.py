3a11ba16ddfb221b4ea61c6463d8c6d6a6f85b8e
Chris Adams <chris@improbable.org>
2010-01-21 16:43:45 -0500

    Simplified South migration rules for JSONField

--- feincms/contrib/fields.py
+++ feincms/contrib/fields.py
@@ -51,24 +51,8 @@ class JSONField(models.TextField):
 
 try:
     from south.modelsinspector import add_introspection_rules
-    from django.db.models.fields import NOT_PROVIDED
-    from django.conf import settings
 
-    JSONField_introspection_rule = (
-            (JSONField,),
-            [],
-            {
-            "null": ["null", {"default": False}],
-            "blank": ["blank", {"default": False, "ignore_if":"primary_key"}],
-            "primary_key": ["primary_key", {"default": False}],
-            "max_length": ["max_length", {"default": None}],
-            "unique": ["_unique", {"default": False}],
-            "db_index": ["db_index", {"default": False}],
-            "default": ["default", {"default": NOT_PROVIDED}],
-            "db_column": ["db_column", {"default": None}],
-            "db_tablespace": ["db_tablespace", {"default": settings.DEFAULT_INDEX_TABLESPACE}],
-            },
-    )
+    JSONField_introspection_rule = ( (JSONField,), [], {}, )
 
     add_introspection_rules(rules=[JSONField_introspection_rule], patterns=["^feincms\.contrib\.fields"])
 except ImportError:
