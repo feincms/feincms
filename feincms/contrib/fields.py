21f78996e8d61f91e30103aea98e86d64fa16362
Chris Adams <chris@improbable.org>
2010-01-21 16:11:13 -0500

    Add South introspection rule for contrib.fields.JSONField

--- feincms/contrib/fields.py
+++ feincms/contrib/fields.py
@@ -48,3 +48,28 @@ class JSONField(models.TextField):
             value = json.dumps(value, cls=DjangoJSONEncoder)
 
         return super(JSONField, self).get_db_prep_save(value)
+
+try:
+    from south.modelsinspector import add_introspection_rules
+    from django.db.models.fields import NOT_PROVIDED
+    from django.conf import settings
+
+    JSONField_introspection_rule = (
+            (JSONField,),
+            [],
+            {
+            "null": ["null", {"default": False}],
+            "blank": ["blank", {"default": False, "ignore_if":"primary_key"}],
+            "primary_key": ["primary_key", {"default": False}],
+            "max_length": ["max_length", {"default": None}],
+            "unique": ["_unique", {"default": False}],
+            "db_index": ["db_index", {"default": False}],
+            "default": ["default", {"default": NOT_PROVIDED}],
+            "db_column": ["db_column", {"default": None}],
+            "db_tablespace": ["db_tablespace", {"default": settings.DEFAULT_INDEX_TABLESPACE}],
+            },
+    )
+
+    add_introspection_rules(rules=[JSONField_introspection_rule], patterns=["^feincms\.contrib\.fields"])
+except ImportError:
+    pass
