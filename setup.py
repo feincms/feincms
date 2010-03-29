49bb240fe29387b03d1f22faffdddded9332a0f3
Matthias Kestenholz <mk@spinlock.ch>
2010-03-29 15:31:35 +0200

    Point all django-mptt links to my fork
    While at it, fix an oversight in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.dist import Distribution
 
 Distribution({
     "setup_requires": [
-        'Django >=1.1.1'
+        #'Django >=1.1.1'
     ]
 })
 
@@ -39,7 +39,7 @@ setup(name='FeinCMS',
     requires=[
         #'lxml', # only needed for rich text cleansing
         'tagging (>0.2.1)', # please use SVN trunk
-        'django_mptt (>0.2.1)', # please use SVN trunk
+        'django_mptt (>0.2.1)', # please use the version from http://github.com/matthiask/django-mptt/
     ],
     packages=['feincms',
         'feincms.admin',
