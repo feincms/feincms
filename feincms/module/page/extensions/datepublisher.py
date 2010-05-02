32775773ba113b65e2237546653c6b7a9d9937a6
mjl <mjl@laubach.at>
2010-05-02 20:32:42 +0200

    Make the datepublisher extension a bit less dependent on the "Page" model.
    It now works with any Base that has at least a "active_filters" in its manager.

--- feincms/module/page/extensions/datepublisher.py
+++ feincms/module/page/extensions/datepublisher.py
@@ -2,7 +2,11 @@
 Allows setting a date range for when the page is active. Modifies the active()
 manager method so that only pages inside the given range are used in the default
 views and the template tags.
+
+Depends on the page class having a "active_filters" list that will be used by
+the page's manager to determine which entries are to be considered active.
 """
+# ------------------------------------------------------------------------
 
 from datetime import datetime
 
@@ -10,7 +14,7 @@ from django.db import models
 from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
-
+# ------------------------------------------------------------------------
 def format_date(d, if_none=''):
     """
     Format a date in a nice human readable way: Omit the year if it's the current
@@ -23,8 +27,7 @@ def format_date(d, if_none=''):
     fmt = (d.year == now.year) and '%d.%m' or '%d.%m.%Y'
     return d.strftime(fmt)
 
-
-
+# ------------------------------------------------------------------------
 def register(cls, admin_cls):
     cls.add_to_class('publication_date', models.DateTimeField(_('publication date'),
         default=datetime.now))
@@ -45,5 +48,11 @@ def register(cls, admin_cls):
     datepublisher_admin.short_description = _('visible from - to')
 
     admin_cls.datepublisher_admin = datepublisher_admin
-    admin_cls.list_display.insert(admin_cls.list_display.index('is_visible_admin') + 1,
-                                  'datepublisher_admin')
+    try:
+        pos = admin_cls.list_display.index('is_visible_admin')
+    except ValueError:
+        pos = len(admin_cls.list_display)
+
+    admin_cls.list_display.insert(pos + 1, 'datepublisher_admin')
+
+# ------------------------------------------------------------------------
