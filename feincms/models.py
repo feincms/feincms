306833018568685e22555b3619ff7935b2ec306f
Matthias Kestenholz <mk@spinlock.ch>
2010-05-19 13:13:53 +0200

    Make ContentProxy class configurable

--- feincms/models.py
+++ feincms/models.py
@@ -79,10 +79,58 @@ class Template(object):
         return force_unicode(self.title)
 
 
+class ContentProxy(object):
+    """
+    This proxy offers attribute-style access to the page contents of regions::
+
+        >> page = Page.objects.all()[0]
+        >> page.content.main
+        [A list of all page contents which are assigned to the region with key 'main']
+    """
+
+    def __init__(self, item):
+        self.item = item
+
+    def __getattr__(self, attr):
+        """
+        Get all item content instances for the specified item and region
+
+        If no item contents could be found for the current item and the region
+        has the inherited flag set, this method will go up the ancestor chain
+        until either some item contents have found or no ancestors are left.
+        """
+        if (attr.startswith('__')):
+            raise AttributeError
+
+        item = self.__dict__['item']
+
+        try:
+            region = item.template.regions_dict[attr]
+        except KeyError:
+            return []
+
+        def collect_items(obj):
+            contents = obj._content_for_region(region)
+
+            # go to parent if this model has a parent attribute
+            # TODO: this should be abstracted into a property/method or something
+            # The link which should be followed is not always '.parent'
+            if region.inherited and not contents and hasattr(obj, 'parent_id') and obj.parent_id:
+                return collect_items(obj.parent)
+
+            return contents
+
+        contents = collect_items(item)
+        contents.sort(key=lambda c: c.ordering)
+        return contents
+
+
 class Base(models.Model):
     """
     This is the base class for your CMS models.
     """
+
+    content_proxy_class = ContentProxy
 
     class Meta:
         abstract = True
@@ -210,7 +258,7 @@ class Base(models.Model):
         """
 
         if not hasattr(self, '_content_proxy'):
-            self._content_proxy = ContentProxy(self)
+            self._content_proxy = self.content_proxy_class(self)
 
         return self._content_proxy
 
@@ -547,50 +595,3 @@ class Base(models.Model):
         for cls in self._feincms_content_types:
             cls.objects.filter(parent=self).delete()
         self.copy_content_from(obj)
-
-
-class ContentProxy(object):
-    """
-    This proxy offers attribute-style access to the page contents of regions::
-
-        >> page = Page.objects.all()[0]
-        >> page.content.main
-        [A list of all page contents which are assigned to the region with key 'main']
-    """
-
-    def __init__(self, item):
-        self.item = item
-
-    def __getattr__(self, attr):
-        """
-        Get all item content instances for the specified item and region
-
-        If no item contents could be found for the current item and the region
-        has the inherited flag set, this method will go up the ancestor chain
-        until either some item contents have found or no ancestors are left.
-        """
-        if (attr.startswith('__')):
-            raise AttributeError
-
-        item = self.__dict__['item']
-
-        try:
-            region = item.template.regions_dict[attr]
-        except KeyError:
-            return []
-
-        def collect_items(obj):
-            contents = obj._content_for_region(region)
-
-            # go to parent if this model has a parent attribute
-            # TODO: this should be abstracted into a property/method or something
-            # The link which should be followed is not always '.parent'
-            if region.inherited and not contents and hasattr(obj, 'parent_id') and obj.parent_id:
-                return collect_items(obj.parent)
-
-            return contents
-
-        contents = collect_items(item)
-        contents.sort(key=lambda c: c.ordering)
-        return contents
-
