feincms/feincms

commit b0012e60aec84cdb09367447ebed94cc374a99c6

In datepublisher, round the pub and pub_end dates too.
Otherwise, a freshly created page would be considered inactive for a while.

feincms/module/page/extensions/datepublisher.py
@@ -43,11 +43,21 @@ def granular_now(n=None):
 # ------------------------------------------------------------------------
 def register(cls, admin_cls):
     cls.add_to_class('publication_date', models.DateTimeField(_('publication date'),
-        default=datetime.now))
+        default=granular_now))
     cls.add_to_class('publication_end_date', models.DateTimeField(_('publication end date'),
         blank=True, null=True,
         help_text=_('Leave empty if the entry should stay active forever.')))
 
+    # Patch in rounding the pub and pub_end dates on save
+    def granular_save(obj, *args, **kwargs):
+        if obj.publication_date:
+            obj.publication_date = granular_now(obj.publication_date)
+        if obj.publication_end_date:
+            obj.publication_end_date = granular_now(obj.publication_end_date)
+        super(cls, obj).save(*args, **kwargs)
+    cls.save = granular_save
+
+    # Append publication date active check
     if hasattr(cls.objects, 'add_to_active_filters'):
         cls.objects.add_to_active_filters(
             Q(publication_date__lte=granular_now) &

commit b36ca6181c1a194390ad67e816069ff0b951362e

Correct bug introduced in b0012e60aec84cdb09367447ebed94cc374a99c6

feincms/module/page/extensions/datepublisher.py
@@ -49,12 +49,13 @@ def register(cls, admin_cls):
         help_text=_('Leave empty if the entry should stay active forever.')))
 
     # Patch in rounding the pub and pub_end dates on save
+    orig_save = cls.save
     def granular_save(obj, *args, **kwargs):
         if obj.publication_date:
             obj.publication_date = granular_now(obj.publication_date)
         if obj.publication_end_date:
             obj.publication_end_date = granular_now(obj.publication_end_date)
-        super(cls, obj).save(*args, **kwargs)
+        orig_save(obj, *args, **kwargs)
     cls.save = granular_save
 
     # Append publication date active check