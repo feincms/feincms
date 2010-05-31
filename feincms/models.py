22b5642e746846ce8ebe7bfd4844a9b340e0ea9d
Matthias Kestenholz <mk@spinlock.ch>
2010-05-31 11:53:38 +0200

    Include _meta hack description and reasoning inside a comment
    Thanks to mjl for the suggestion.

--- feincms/models.py
+++ feincms/models.py
@@ -479,9 +479,24 @@ class Base(models.Model):
             for key, includes in model.feincms_item_editor_includes.items():
                 cls.feincms_item_editor_includes.setdefault(key, set()).update(includes)
 
-        # Ensure meta information is up-to-date
-        # See issue #63 on github or more informations:
-        # http://github.com/matthiask/feincms/issues/issue/63/#issue/63/comment/256443
+        # Ensure meta information concerning related fields is up-to-date.
+        #
+        # Upon accessing the related fields information from Model._meta, the related
+        # fields are cached and never refreshed again (because models and model relations
+        # are defined upon import time, if you do not fumble around with models like we
+        # do right here.)
+        #
+        # Adding related models after this information has been cached leads to models
+        # not knowing about related items, which again causes the bug we had in
+        # issue #63 on github:
+        #
+        # http://github.com/matthiask/feincms/issues/issue/63/
+        #
+        # Currently, all methods filling up the Model.meta cache start with fill_.
+        # We call all these methods upon creation of a new content type to make sure
+        # that we really really do not forget a relation somewhere. Of course, we do
+        # too much here, but better a bit too much upon application startup than not
+        # enough like before.
         for fn in [s for s in dir(cls._meta) if s[:6]=='_fill_']:
             getattr(cls._meta, fn)()
 
