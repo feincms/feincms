feincms/feincms

commit 2d299f05fd6339fa001ec31ada48426ba234b913

Fix IOError when removing translations from images

feincms/module/medialibrary/models.py
@@ -211,7 +211,7 @@ class MediaFileBase(Base, TranslatedObjectMixin):
         # Try to detect things that are not really images
         if self.type == 'image':
             try:
-                Image.open(self.file)
+                Image.open(self.file.path)
             except:
                 self.type = self.determine_file_type('***') # It's binary something
 

commit 0bc69465802e2fa248366e12b7d9f59c12402b01

Check if mediafile was already saved
This fixes a bug introduced in 2d299f05fd6339fa001ec31ada48426ba234b913
where every image was marked as binary (which was a fix for
http://github.com/matthiask/feincms/issues/closed#issue/71. The problem was
that self.file no longer points to the file object once the item has
already been saved.

feincms/module/medialibrary/models.py
@@ -209,9 +209,9 @@ class MediaFileBase(Base, TranslatedObjectMixin):
 
         self.type = self.determine_file_type(self.file.name)
         # Try to detect things that are not really images
-        if self.type == 'image':
+        if self.type == 'image' and not self.id:
             try:
-                Image.open(self.file.path)
+                Image.open(self.file)
             except:
                 self.type = self.determine_file_type('***') # It's binary something
 