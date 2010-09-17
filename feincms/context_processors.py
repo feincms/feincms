13bd6ceb23da4766a5b3a185f31f48aab67e25ab
Andrew D. Ball <anball@gmail.com>
2010-09-17 16:43:03 -0400

    make the comments a bit more kosher grammatically

--- feincms/context_processors.py
+++ feincms/context_processors.py
@@ -2,8 +2,8 @@ from feincms.module.page.models import Page
 
 
 def add_page_if_missing(request):
-    # If this attribute exists, the a page object has been registered already
-    # by some other part of the code. We let it decide, which page object it
+    # If this attribute exists, then a page object has been registered already
+    # by some other part of the code. We let it decide which page object it
     # wants to pass into the template
     if hasattr(request, '_feincms_page'):
         return {}
