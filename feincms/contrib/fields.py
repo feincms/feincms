459c40f0467b674fd438d2407ace1709db2aa3fc
Christof Franke <kraut@wurzt.de>
2010-11-05 16:36:38 +0100

    added RawIdWidget for tags. Better would be to use TagAutocomplete, i use it in elephantblog extension. Merge of this will follow ;)
    There was an issue with following import syntax:
    import ..tagging (not working with python 2.4)
    so renaming it would bettern than this ugly dot notation ;)

--- feincms/contrib/fields.py
+++ feincms/contrib/fields.py
@@ -83,3 +83,59 @@ try:
     add_introspection_rules(rules=[JSONField_introspection_rule], patterns=["^feincms\.contrib\.fields"])
 except ImportError:
     pass
+
+# taken from http://djangosnippets.org/snippets/2217/
+# 17.10.2010
+from django import forms
+from django.contrib import admin
+from django.contrib.admin.widgets import ManyToManyRawIdWidget, ForeignKeyRawIdWidget
+from django.core.urlresolvers import reverse
+from django.utils.encoding import smart_unicode
+from django.utils.html import escape
+
+class VerboseForeignKeyRawIdWidget(ForeignKeyRawIdWidget):
+    def label_for_value(self, value):
+        key = self.rel.get_related_field().name
+        try:
+            obj = self.rel.to._default_manager.using(self.db).get(**{key: value})
+            change_url = reverse(
+                "admin:%s_%s_change" % (obj._meta.app_label, obj._meta.object_name.lower()),
+                args=(obj.pk,)
+            )
+            return '&nbsp;<strong><a href="%s">%s</a></strong>' % (change_url, escape(obj))
+        except (ValueError, self.rel.to.DoesNotExist):
+            return ''
+
+class VerboseManyToManyRawIdWidget(ManyToManyRawIdWidget):
+    def label_for_value(self, value):
+        values = value.split(',')
+        str_values = ['<ul>',]
+        key = self.rel.get_related_field().name
+        for v in values:
+            try:
+                obj = self.rel.to._default_manager.using(self.db).get(**{key: v})
+                x = smart_unicode(obj)
+                change_url = reverse(
+                    "admin:%s_%s_change" % (obj._meta.app_label, obj._meta.object_name.lower()),
+                    args=(obj.pk,)
+                )
+                str_values += ['<li><strong><a href="%s">%s</a></strong></li>' % (change_url, escape(x))]
+            except self.rel.to.DoesNotExist:
+                str_values += [u'???']
+        
+        str_values += ['</ul>',]
+        return u' '.join(str_values)
+
+class ImproveRawIdFieldsForm(admin.ModelAdmin):
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name in self.raw_id_fields:
+            kwargs.pop("request", None)
+            type = db_field.rel.__class__.__name__
+            if type == "ManyToOneRel":
+                kwargs['widget'] = VerboseForeignKeyRawIdWidget(db_field.rel)
+            elif type == "ManyToManyRel":
+                kwargs['widget'] = VerboseManyToManyRawIdWidget(db_field.rel)
+            return db_field.formfield(**kwargs)
+        return super(ImproveRawIdFieldsForm, self).formfield_for_dbfield(db_field, **kwargs)
+
+
