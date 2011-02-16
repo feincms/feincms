c05ec70d685e675dc868e38234988a0bd2c38edd
Martin J. Laubach <mjl@laubach.at>
2011-02-16 17:41:56 +0100

    Add array.map function for IE.

--- feincms/media/feincms/ie_compat.js
+++ feincms/media/feincms/ie_compat.js
@@ -38,3 +38,23 @@ if (!Array.prototype.filter)
         return res;
     };
 }
+
+if (!Array.prototype.map)
+{
+  Array.prototype.map = function(fun /*, thisp*/)
+  {
+    var len = this.length;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = new Array(len);
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+        res[i] = fun.call(thisp, this[i], i, this);
+    }
+
+    return res;
+  };
+}
