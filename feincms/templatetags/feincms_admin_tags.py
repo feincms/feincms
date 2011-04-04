4b78b177459d3014a5fff494ebbddf18a8f6c73a
Martin J. Laubach <mjl@laubach.at>
2011-04-04 14:30:19 +0200

    Don't crash the admin if an inline for the item editor does not have a verbose_name attribute.

--- feincms/templatetags/feincms_admin_tags.py
+++ feincms/templatetags/feincms_admin_tags.py
@@ -11,11 +11,12 @@ def post_process_fieldsets(fieldset):
     ``id``, ``DELETE`` and ``ORDER`` currently.
     """
 
-    process = fieldset.model_admin.verbose_name_plural.startswith('Feincms_Inline:')
-    if process:
-        # Exclude special fields and the primary key
-        excluded_fields = ('id', 'DELETE', 'ORDER')
-        fieldset.fields = [f for f in fieldset.form.fields.keys() if f not in excluded_fields]
+    if hasattr(fieldset.model_admin, 'verbose_name'):
+        process = fieldset.model_admin.verbose_name_plural.startswith('Feincms_Inline:')
+        if process:
+            # Exclude special fields and the primary key
+            excluded_fields = ('id', 'DELETE', 'ORDER')
+            fieldset.fields = [f for f in fieldset.form.fields.keys() if f not in excluded_fields]
 
     for line in fieldset:
         yield line
