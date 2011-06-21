55bc355fc97eb5e034e86e7c55919d8cca0edb2b
Matthias Kestenholz <mk@spinlock.ch>
2011-06-21 18:34:24 +0200

    Remove deprecated appcontent_parameters context processor
    It did nothing for some time anyway.

--- feincms/context_processors.py
+++ feincms/context_processors.py
@@ -17,8 +17,3 @@ def add_page_if_missing(request):
             }
     except Page.DoesNotExist:
         return {}
-
-
-def appcontent_parameters(request):
-    # Remove in FeinCMS 1.4.
-    return {}
