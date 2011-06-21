49c1f981c2840779142165f085a11c1c2da5854c
Matthias Kestenholz <mk@spinlock.ch>
2011-06-21 18:22:47 +0200

    Move request and response processors into their own module

--- feincms/module/page/models.py
+++ feincms/module/page/models.py
@@ -35,6 +35,7 @@ from feincms.admin import editor
 from feincms.admin import item_editor
 from feincms.management.checker import check_database_schema
 from feincms.models import Base, create_base_model
+from feincms.module.page import processors
 from feincms.utils import get_object, copy_model_instance
 import feincms.admin.filterspecs
 
@@ -487,119 +488,12 @@ class Page(Base):
         for fn in self.response_processors:
             fn(self, request, response)
 
-    def require_path_active_request_processor(self, request):
-        """
-        Checks whether any ancestors are actually inaccessible (ie. not
-        inactive or expired) and raise a 404 if so.
-        """
-        if not self.are_ancestors_active():
-            raise Http404()
-
     def get_redirect_to_target(self, request):
         """
         This might be overriden/extended by extension modules.
         """
         return self.redirect_to
 
-    def redirect_request_processor(self, request):
-        target = self.get_redirect_to_target(request)
-        if target:
-            return HttpResponseRedirect(target)
-
-    def frontendediting_request_processor(self, request):
-        if not 'frontend_editing' in request.GET:
-            return
-
-        if request.user.has_module_perms('page'):
-            try:
-                enable_fe = int(request.GET['frontend_editing']) > 0
-            except ValueError:
-                enable_fe = False
-
-            request.session['frontend_editing'] = enable_fe
-
-        # Redirect to cleanup URLs
-        return HttpResponseRedirect(request.path)
-
-    def etag_request_processor(self, request):
-
-        # XXX is this a performance concern? Does it create a new class
-        # every time the processor is called or is this optimized to a static
-        # class??
-        class DummyResponse(dict):
-            """
-            This is a dummy class with enough behaviour of HttpResponse so we
-            can use the condition decorator without too much pain.
-            """
-            def has_header(self, what):
-                return False
-
-        def dummy_response_handler(*args, **kwargs):
-            return DummyResponse()
-
-        def etagger(request, page, *args, **kwargs):
-            etag = page.etag(request)
-            return etag
-
-        def lastmodifier(request, page, *args, **kwargs):
-            lm = page.last_modified()
-            return lm
-
-        # Unavailable in Django 1.0 -- the current implementation of ETag support
-        # requires Django 1.1 unfortunately.
-        from django.views.decorators.http import condition
-
-        # Now wrap the condition decorator around our dummy handler:
-        # the net effect is that we will be getting a DummyResponse from
-        # the handler if processing is to continue and a non-DummyResponse
-        # (should be a "304 not modified") if the etag matches.
-        rsp = condition(etag_func=etagger, last_modified_func=lastmodifier)(dummy_response_handler)(request, self)
-
-        # If dummy then don't do anything, if a real response, return and
-        # thus shortcut the request processing.
-        if not isinstance(rsp, DummyResponse):
-            return rsp
-
-    def etag_response_processor(self, request, response):
-        """
-        Response processor to set an etag header on outgoing responses.
-        The Page.etag() method must return something valid as etag content
-        whenever you want an etag header generated.
-        """
-        etag = self.etag(request)
-        if etag is not None:
-            response['ETag'] = '"' + etag + '"'
-
-    @staticmethod
-    def debug_sql_queries_response_processor(verbose=False, file=sys.stderr):
-        if not django_settings.DEBUG:
-            return lambda self, request, response: None
-
-        def processor(self, request, response):
-            from django.db import connection
-
-            print_sql = lambda x: x
-            try:
-                import sqlparse
-                print_sql = lambda x: sqlparse.format(x, reindent=True, keyword_case='upper')
-            except:
-                pass
-
-            if verbose:
-                print >> file, "--------------------------------------------------------------"
-            time = 0.0
-            i = 0
-            for q in connection.queries:
-                i += 1
-                if verbose:
-                    print >> file, "%d : [%s]\n%s\n" % ( i, q['time'], print_sql(q['sql']))
-                time += float(q['time'])
-
-            print >> file, "--------------------------------------------------------------"
-            print >> file, "Total: %d queries, %.3f ms" % (i, time)
-            print >> file, "--------------------------------------------------------------"
-
-        return processor
 
     @classmethod
     def register_request_processors(cls, *processors):
@@ -630,9 +524,12 @@ if mptt_register: # MPTT 0.3 legacy support
     mptt.register(Page)
 
 # Our default request processors
-Page.register_request_processors(Page.require_path_active_request_processor,
-                                 Page.frontendediting_request_processor,
-                                 Page.redirect_request_processor)
+Page.register_request_processors(processors.require_path_active_request_processor,
+                                 processors.redirect_request_processor)
+
+if settings.FEINCMS_FRONTEND_EDITING:
+    Page.register_request_processors(
+        processors.frontendediting_request_processor)
 
 signals.post_syncdb.connect(check_database_schema(Page, __name__), weak=False)
 
