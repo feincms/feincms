e0dae3362a3a1826ceaa620d417ebb323dfaf829
Matthias Kestenholz <mk@spinlock.ch>
2011-08-08 10:27:30 +0200

    Add staticfiles context processor to example

--- example/settings.py
+++ example/settings.py
@@ -47,6 +47,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
+    'django.core.context_processors.static',
 )
 
 TEMPLATE_LOADERS = (
