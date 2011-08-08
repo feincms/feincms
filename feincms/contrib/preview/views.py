fe916dffe41a7c717e9b91bf6bd55aa1a9053db3
Matthias Kestenholz <mk@spinlock.ch>
2011-08-08 10:21:46 +0200

    Class-based preview view actually works now

--- feincms/contrib/preview/views.py
+++ feincms/contrib/preview/views.py
@@ -19,17 +19,17 @@ class PreviewHandler(Handler):
         if not request.user.is_staff:
             raise Http404
 
-        page = get_object_or_404(Page, pk=page_id)
+        self.page = get_object_or_404(Page, pk=page_id)
 
         # Throw out request processor which will cause the page to-be-previewed
         # to be seen as inactive (which is the case, of course)
-        page.request_processors = [rp for rp in Page.request_processors if rp not in (
+        self.page.request_processors = [rp for rp in Page.request_processors if rp not in (
             processors.require_path_active_request_processor,)]
 
         # Remove _preview/42/ from URL, the rest of the handler code should not
         # know that anything about previewing. Handler.prepare will still raise
         # a 404 if the extra_path isn't consumed by any content type
-        request.path = page.get_absolute_url()
+        request.path = self.page.get_absolute_url()
 
         response = self.prepare()
         if response:
