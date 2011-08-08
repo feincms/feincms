a66cff7d14991d211856fa6282994ede65759bc4
Matthias Kestenholz <mk@spinlock.ch>
2011-08-08 10:13:52 +0200

    Fix a few blunders

--- feincms/views/base.py
+++ feincms/views/base.py
@@ -1,5 +1,5 @@
 import warnings
-warnings.warning('feincms.views.base is deprecated. Please use feincms.views.legacy if '
+warnings.warn('feincms.views.base is deprecated. Please use feincms.views.legacy if '
     'you want to keep on using the old handler. Otherwise, it is advised to switch '
     'to the class-based view (Django >= 1.3 only).',
     DeprecationWarning)
