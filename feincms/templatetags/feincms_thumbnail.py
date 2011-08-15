bb293293a6ecdb2a41b6ee872fdd4b148e00fcec
Matthias Kestenholz <mk@spinlock.ch>
2011-08-15 17:22:00 +0200

    Refactor thumbnail filters a bit

--- feincms/templatetags/feincms_thumbnail.py
+++ feincms/templatetags/feincms_thumbnail.py
@@ -19,75 +19,71 @@ from django.core.files.base import ContentFile
 register = template.Library()
 
 
-THUMBNAIL_SIZE_RE = re.compile(r'^(?P<w>\d+)x(?P<h>\d+)$')
+class Thumbnailer(object):
+    THUMBNAIL_SIZE_RE = re.compile(r'^(?P<w>\d+)x(?P<h>\d+)$')
+    MARKER = '_thumb_'
 
-@register.filter
-def thumbnail(filename, size='200x200'):
-    """
-    Creates a thumbnail from the image passed, returning its path::
+    def __init__(self, filename, size='200x200'):
+        self.filename = filename
+        self.size = size
 
-        {{ object.image|thumbnail:"400x300" }}
-    OR
-        {{ object.image.name|thumbnail:"400x300" }}
+    @property
+    def url(self):
+        return unicode(self)
 
-    You can pass either an ``ImageField``, ``FileField`` or the ``name``
-    but not the ``url`` attribute of an ``ImageField`` or ``FileField``.
+    def __unicode__(self):
+        match = self.THUMBNAIL_SIZE_RE.match(self.size)
+        if not (self.filename and match):
+            return u''
 
-    The dimensions passed are treated as a bounding box. The aspect ratio of
-    the initial image is preserved. Images aren't blown up in size if they
-    are already smaller.
+        matches = match.groupdict()
 
-    Both width and height must be specified. If you do not care about one
-    of them, just set it to an arbitrarily large number::
+        # figure out storage
+        if hasattr(self.filename, 'storage'):
+            storage = self.filename.storage
+        else:
+            storage = default_storage
 
-        {{ object.image|thumbnail:"300x999999" }}
-    """
+        # figure out name
+        if hasattr(self.filename, 'name'):
+            filename = self.filename.name
+        else:
+            filename = force_unicode(self.filename)
 
-    match = THUMBNAIL_SIZE_RE.match(size)
-
-    if not (filename and match):
-        # Better return empty than crash
-        return u''
-
-    matches = match.groupdict()
-
-    # figure out storage
-    if hasattr(filename, 'storage'):
-        storage = filename.storage
-    else:
-        storage = default_storage
-
-    # figure out name
-    if hasattr(filename, 'name'):
-        filename = filename.name
-    else:
-        filename = force_unicode(filename)
-
-    # defining the size
-    w, h = int(matches['w']), int(matches['h'])
-
-    # defining the filename and the miniature filename
-    try:
-        basename, format = filename.rsplit('.', 1)
-    except ValueError:
-        basename, format = filename, 'jpg'
-    miniature = basename + '_thumb_' + size + '.' +  format
-
-    if not storage.exists(miniature):
-        generate = True
-    else:
+        # defining the filename and the miniature filename
         try:
-            generate = storage.modified_time(miniature)<storage.modified_time(filename)
-        except (NotImplementedError, AttributeError):
-            # storage does NOT support modified_time
-            generate = False
+            basename, format = filename.rsplit('.', 1)
+        except ValueError:
+            basename, format = filename, 'jpg'
+        miniature = basename + self.MARKER + self.size + '.' +  format
 
-    if generate:
+        if not storage.exists(miniature):
+            generate = True
+        else:
+            try:
+                generate = storage.modified_time(miniature) < storage.modified_time(filename)
+            except (NotImplementedError, AttributeError):
+                # storage does NOT support modified_time
+                generate = False
+
+        if generate:
+            return self.generate(
+                storage=storage,
+                original=filename,
+                size=matches,
+                miniature=miniature)
+
+        return storage.url(miniature)
+
+    def generate(self, storage, original, size, miniature):
         try:
-            image = Image.open(StringIO(storage.open(filename).read()))
+            image = Image.open(StringIO(storage.open(original).read()))
         except IOError:
              # Do not crash if file does not exist for some reason
-            return storage.url(filename)
+            return storage.url(original)
+
+        # defining the size
+        w, h = int(size['w']), int(size['h'])
 
         image.thumbnail([w, h], Image.ANTIALIAS)
         buf = StringIO()
@@ -98,66 +94,26 @@ def thumbnail(filename, size='200x200'):
         buf.close()
         storage.save(miniature, ContentFile(raw_data))
 
-    return storage.url(miniature)
+        return storage.url(miniature)
 
 
-CROPSCALE_SIZE_RE = re.compile(r'^(?P<w>\d+)x(?P<h>\d+)(-(?P<x>\d+)x(?P<y>\d+))?$')
+class CropscaleThumbnailer(Thumbnailer):
+    THUMBNAIL_SIZE_RE = re.compile(r'^(?P<w>\d+)x(?P<h>\d+)(-(?P<x>\d+)x(?P<y>\d+))?$')
+    MARKER = '_cropscale_'
 
-@register.filter
-def cropscale(filename, size='200x200'):
-    """
-    Scales the image down and crops it so that its size equals exactly the size
-    passed (as long as the initial image is bigger than the specification).
-    """
-
-    match = CROPSCALE_SIZE_RE.match(size)
-
-    if not (filename and match):
-        # Better return empty than crash
-        return u''
-
-    matches = match.groupdict()
-
-    # figure out storage
-    if hasattr(filename, 'storage'):
-        storage = filename.storage
-    else:
-        storage = default_storage
-
-    # figure out name
-    if hasattr(filename, 'name'):
-        filename = filename.name
-    else:
-        filename = force_unicode(filename)
-
-    w, h = int(matches['w']), int(matches['h'])
-
-    if matches['x'] and matches['y']:
-        x, y = int(matches['x']), int(matches['y'])
-    else:
-        x, y = 50, 50
-
-    try:
-        basename, format = filename.rsplit('.', 1)
-    except ValueError:
-        basename, format = filename, 'jpg'
-    miniature = basename + '_cropscale_' + size + '.' +  format
-
-    if not storage.exists(miniature):
-        generate = True
-    else:
+    def generate(self, storage, original, size, miniature):
         try:
-            generate = storage.modified_time(miniature)<storage.modified_time(filename)
-        except (NotImplementedError, AttributeError):
-            # storage does NOT support modified_time
-            generate = False
-
-    if generate:
-        try:
-            image = Image.open(StringIO(storage.open(filename).read()))
+            image = Image.open(StringIO(storage.open(original).read()))
         except IOError:
              # Do not crash if file does not exist for some reason
-            return storage.url(filename)
+            return storage.url(original)
+
+        w, h = int(size['w']), int(size['h'])
+
+        if size['x'] and size['y']:
+            x, y = int(size['x']), int(size['y'])
+        else:
+            x, y = 50, 50
 
         src_width, src_height = image.size
         src_ratio = float(src_width) / float(src_height)
@@ -186,4 +142,39 @@ def cropscale(filename, size='200x200'):
         buf.close()
         storage.save(miniature, ContentFile(raw_data))
 
-    return storage.url(miniature)
+        return storage.url(miniature)
+
+
+@register.filter
+def thumbnail(filename, size='200x200'):
+    """
+    Creates a thumbnail from the image passed, returning its path::
+
+        {{ object.image|thumbnail:"400x300" }}
+    OR
+        {{ object.image.name|thumbnail:"400x300" }}
+
+    You can pass either an ``ImageField``, ``FileField`` or the ``name``
+    but not the ``url`` attribute of an ``ImageField`` or ``FileField``.
+
+    The dimensions passed are treated as a bounding box. The aspect ratio of
+    the initial image is preserved. Images aren't blown up in size if they
+    are already smaller.
+
+    Both width and height must be specified. If you do not care about one
+    of them, just set it to an arbitrarily large number::
+
+        {{ object.image|thumbnail:"300x999999" }}
+    """
+
+    return Thumbnailer(filename, size)
+
+
+@register.filter
+def cropscale(filename, size='200x200'):
+    """
+    Scales the image down and crops it so that its size equals exactly the size
+    passed (as long as the initial image is bigger than the specification).
+    """
+
+    return CropscaleThumbnailer(filename, size)
