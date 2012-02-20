1cf38456b5391b5533200aa8efda0d7357ff21fa
Matthias Kestenholz <mk@spinlock.ch>
2012-02-20 13:50:52 +0100

    Remove the deprecated feincms.views.base module
    The code has been moved to feincms.views.legacy last summer.

--- feincms/views/base.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.warn('feincms.views.base is deprecated. Please use feincms.views.legacy if '
-    'you want to keep on using the old handler. Otherwise, it is advised to switch '
-    'to the class-based view (Django >= 1.3 only).',
-    DeprecationWarning, stacklevel=2)
-
-from feincms.views.legacy.views import *
