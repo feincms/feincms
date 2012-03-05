326c7e37d49d01153603dc9327014fad9de49734
Matthias Kestenholz <mk@spinlock.ch>
2012-03-05 08:08:50 +0100

    Try being a better citizen by switching to distutils and requirements.txt

---
+++ setuplib.py
@@ -0,0 +1,50 @@
+import os
+
+
+__all__ = ['find_files']
+
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+
+def find_files(package_dir):
+    """
+    Returns a tuple consisting of a list of packages and a list of data
+    files suitable for passing on to ``distutils.core.setup``
+
+    Requires the folder name containing the package files; ``find_files``
+    assumes that ``setup.py`` is located in the same folder as the folder
+    containing those files.
+
+    Code lifted from Django's ``setup.py``, thanks!
+    """
+
+    # Compile the list of packages available, because distutils doesn't have
+    # an easy way to do this.
+    packages, data_files = [], []
+    root_dir = os.path.dirname(__file__)
+    if root_dir != '':
+        os.chdir(root_dir)
+
+    for dirpath, dirnames, filenames in os.walk(package_dir):
+        # Ignore dirnames that start with '.'
+        for i, dirname in enumerate(dirnames):
+            if dirname.startswith('.'): del dirnames[i]
+        if '__init__.py' in filenames:
+            packages.append('.'.join(fullsplit(dirpath)))
+        elif filenames:
+            data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+    return packages, data_files
