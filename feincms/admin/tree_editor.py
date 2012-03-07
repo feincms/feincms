444fb93b0b27e5f370acfba2c69500655acd24a8
Michael Bashkirov <bashmish@gmail.com>
2012-03-07 15:16:29 +0400

    Fall back to the old tree manager location if move_node doesn't exist.

--- feincms/admin/tree_editor.py
+++ feincms/admin/tree_editor.py
@@ -366,13 +366,18 @@ class TreeEditor(admin.ModelAdmin):
         return r and super(TreeEditor, self).has_delete_permission(request, obj)
 
     def _move_node(self, request):
-        cut_item = self.model.objects.get(pk=request.POST.get('cut_item'))
-        pasted_on = self.model.objects.get(pk=request.POST.get('pasted_on'))
+        if hasattr(self.model.objects, 'move_node'):
+            tree_manager = self.model.objects
+        else:
+            tree_manager = self.model._tree_manager
+
+        cut_item = tree_manager.get(pk=request.POST.get('cut_item'))
+        pasted_on = tree_manager.get(pk=request.POST.get('pasted_on'))
         position = request.POST.get('position')
 
         if position in ('last-child', 'left'):
             try:
-                self.model.objects.move_node(cut_item, pasted_on, position)
+                tree_manager.move_node(cut_item, pasted_on, position)
             except InvalidMove, e:
                 self.message_user(request, unicode(e))
                 return HttpResponse('FAIL')
