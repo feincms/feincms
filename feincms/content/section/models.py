f6cf375f265754f33d048291fafb767ea9aed108
Matthias Kestenholz <mk@spinlock.ch>
2012-04-11 15:14:48 +0200

    Update the SectionContent to current FeinCMS best practices

--- feincms/content/section/models.py
+++ feincms/content/section/models.py
@@ -1,17 +1,21 @@
 from django import forms
 from django.conf import settings as django_settings
-from django.contrib.admin.widgets import AdminRadioSelect
+from django.contrib import admin
 from django.core.exceptions import ImproperlyConfigured
 from django.db import models
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
 from feincms import settings
-from feincms.admin.item_editor import ItemEditorForm
+from feincms.admin.item_editor import FeinCMSInline
 from feincms.contrib.richtext import RichTextField
+from feincms.module.medialibrary.fields import MediaFileForeignKey
 from feincms.module.medialibrary.models import MediaFile
 
-from feincms.content.medialibrary.models import MediaFileWidget
+
+class SectionContentInline(FeinCMSInline):
+    raw_id_fields = ('mediafile',)
+    radio_fields = {'type': admin.VERTICAL}
 
 
 class SectionContent(models.Model):
@@ -19,18 +23,18 @@ class SectionContent(models.Model):
     Title, media file and rich text fields in one content block.
     """
 
+    feincms_item_editor_inline = SectionContentInline
     feincms_item_editor_context_processors = (
         lambda x: settings.FEINCMS_RICHTEXT_INIT_CONTEXT,
     )
     feincms_item_editor_includes = {
-        'head': [
-            settings.FEINCMS_RICHTEXT_INIT_TEMPLATE,
-            'admin/content/mediafile/init.html',
-            ],
-        }
+        'head': [settings.FEINCMS_RICHTEXT_INIT_TEMPLATE],
+    }
 
     title = models.CharField(_('title'), max_length=200, blank=True)
     richtext = RichTextField(_('text'), blank=True)
+    mediafile = MediaFileForeignKey(MediaFile, verbose_name=_('media file'),
+        related_name='+', blank=True, null=True)
 
     class Meta:
         abstract = True
@@ -45,25 +49,18 @@ class SectionContent(models.Model):
         if TYPE_CHOICES is None:
             raise ImproperlyConfigured, 'You need to set TYPE_CHOICES when creating a %s' % cls.__name__
 
-        cls.add_to_class('mediafile', models.ForeignKey(MediaFile, verbose_name=_('media file'),
-            related_name='%s_%s_set' % (cls._meta.app_label, cls._meta.module_name),
-            blank=True, null=True,
-            ))
-
         cls.add_to_class('type', models.CharField(_('type'),
             max_length=10, choices=TYPE_CHOICES,
             default=TYPE_CHOICES[0][0]))
 
-        class MediaFileContentAdminForm(ItemEditorForm):
-            mediafile = forms.ModelChoiceField(queryset=MediaFile.objects.all(),
-                widget=MediaFileWidget, required=False)
-            type = forms.ChoiceField(choices=TYPE_CHOICES,
-                initial=TYPE_CHOICES[0][0], label=_('type'),
-                widget=AdminRadioSelect(attrs={'class': 'radiolist'}))
-
-        cls.feincms_item_editor_form = MediaFileContentAdminForm
-        cls.form = MediaFileContentAdminForm
-        cls.cleanse = cleanse
+        if cleanse:
+            # If cleanse is True use default cleanse method
+            if cleanse == True:
+                from feincms.utils.html.cleanse import cleanse_html
+                cls.cleanse = cleanse_html
+            # Otherwise use passed callable
+            else:
+                cls.cleanse = cleanse
 
     @classmethod
     def get_queryset(cls, filter_args):
@@ -85,6 +82,13 @@ class SectionContent(models.Model):
 
     def save(self, *args, **kwargs):
         if getattr(self, 'cleanse', False):
-            from feincms.utils.html.cleanse import cleanse_html
-            self.richtext = cleanse_html(self.richtext)
+            try:
+                # Passes the rich text content as first argument because
+                # the passed callable has been converted into a bound method
+                self.richtext = self.cleanse(self.text)
+            except TypeError:
+                # Call the original callable, does not pass the rich richtext
+                # content instance along
+                self.richtext = self.cleanse.im_func(self.text)
+
         super(SectionContent, self).save(*args, **kwargs)
