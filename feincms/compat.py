ea75cb3f5dfca8dce48664f9a996b78a30d8d6f7
Matthias Kestenholz <mk@spinlock.ch>
2012-04-24 18:28:32 +0200

    datetime.datetime is a class, members cannot be imported
    Thanks to Cromulent for the report.

--- feincms/compat.py
+++ feincms/compat.py
@@ -2,4 +2,5 @@ try:
     from django.utils.timezone import now as compatible_now
 except ImportError:
     # Django < 1.4 comes without timezone support
-    from datetime.datetime import now as compatible_now
+    from datetime import datetime
+    compatible_now = datetime.now
