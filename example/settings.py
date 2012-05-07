be83f27f5a6e2c693fb7829ebb98df340f6061bd
Matthias Kestenholz <mk@spinlock.ch>
2012-05-07 13:43:04 +0200

    Remove use of old generic views from example

--- example/settings.py
+++ example/settings.py
@@ -41,6 +41,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.request',
     'django.core.context_processors.static',
+
+    'feincms.context_processors.add_page_if_missing',
 )
 
 TEMPLATE_LOADERS = (
