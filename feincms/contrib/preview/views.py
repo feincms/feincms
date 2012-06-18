000d00859ba11f01ab87b3ed8908eedebf95cbf1
Matthias Kestenholz <mk@spinlock.ch>
2012-06-18 11:40:01 +0200

    Remove obsolete replacement of path_active request processor in preview view

--- feincms/contrib/preview/views.py
+++ feincms/contrib/preview/views.py
@@ -20,13 +20,6 @@ class PreviewHandler(Handler):
 
         page = get_object_or_404(Page, pk=page_id)
 
-        # Throw out request processor which will cause the page to-be-previewed
-        # to be seen as inactive (which is the case, of course)
-        page.request_processors = page.request_processors.copy()
-        page.register_request_processor(
-            lambda page, response: None, # Do nothing
-            key='path_active')
-
         # Remove _preview/42/ from URL, the rest of the handler code should not
         # know that anything about previewing. Handler.prepare will still raise
         # a 404 if the extra_path isn't consumed by any content type
