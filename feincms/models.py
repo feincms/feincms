5632c1c372578e92464279172db3077261cba2f9
Greg Turner <greg@interaction.net.au>
2013-02-15 10:46:37 +1100

    Revert commit 3ceb1a37d8927cb6e1e1768eb8452c9b55d950d4. The attempt at elegance caused a bug where reverse relations didn't always end up cached.

--- feincms/models.py
+++ feincms/models.py
@@ -729,10 +729,7 @@ def create_base_model(inherit_from=models.Model):
                 for key, includes in model.feincms_item_editor_includes.items():
                     cls.feincms_item_editor_includes.setdefault(key, set()).update(includes)
 
-            # since this content type is potentially being added after cls is
-            # loaded by Django, we will reload the cls's related objects cache.
-            # See issue #323.
-            cls._meta._fill_related_objects_cache()
+            ensure_completely_loaded(force=True)
             return new_type
 
         @property
