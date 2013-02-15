4d66eb7fb66709291fb01750f790441c5af79577
Matthias Kestenholz <mk@spinlock.ch>
2013-02-15 09:06:10 +0100

    Fix #386: Do not crash in feincms_nav if no extensions registered

--- feincms/module/page/templatetags/feincms_page_tags.py
+++ feincms/module/page/templatetags/feincms_page_tags.py
@@ -87,8 +87,8 @@ def feincms_nav(context, feincms_page, level=1, depth=1):
 
         queryset = _filter(queryset)
 
-    if any((ext in feincms_page._feincms_extensions for ext in (
-            'navigation', 'feincms.module.page.extensions.navigation'))):
+    if any((ext in getattr(feincms_page, '_feincms_extensions', []) for ext
+            in ('navigation', 'feincms.module.page.extensions.navigation'))):
         # Filter out children of nodes which have a navigation extension
         extended_node_rght = [] # mptt node right value
 
