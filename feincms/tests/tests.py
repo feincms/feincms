8a2f2984ba7f2c355c97ac0d661372b3a863af14
Matthias Kestenholz <mk@spinlock.ch>
2013-02-26 20:25:27 +0100

    Remove the cleanse tests because the cleanse module has been removed

--- feincms/tests/tests.py
+++ feincms/tests/tests.py
@@ -158,20 +158,3 @@ class BlogTestCase(TestCase):
         self.create_entries()
         self.assertEqual(self.client.get('/admin/blog/entry/').status_code, 200)
         self.assertEqual(self.client.get('/admin/blog/entry/1/').status_code, 200)
-
-
-class CleanseTestCase(TestCase):
-    def test_01_cleanse(self):
-        from feincms.utils.html.cleanse import cleanse_html
-
-        entries = [
-            (u'<p>&nbsp;</p>', u''),
-            (u'<span style="font-weight: bold;">Something</span><p></p>', u'<strong>Something</strong>'),
-            (u'<p>abc <span>def <em>ghi</em> jkl</span> mno</p>', u'<p>abc def <em>ghi</em> jkl mno</p>'),
-            (u'<span style="font-style: italic;">Something</span><p></p>', u'<em>Something</em>'),
-            (u'<p>abc<br />def</p>', u'<p>abc<br />def</p>'),
-            (u'<p><p><p>&nbsp;</p> </p><p><br /></p></p>', u' '),
-            ]
-
-        for a, b in entries:
-            self.assertEqual(cleanse_html(a), b)
