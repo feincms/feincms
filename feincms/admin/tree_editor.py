9964ca19f051bf305f685790c085258f16700516
Matthias Kestenholz <mk@spinlock.ch>
2013-03-19 09:54:14 +0100

    Take a stab at fixing #395
    Before, moves to the right of an existing node were handled as moves
    to the left.

--- feincms/admin/tree_editor.py
+++ feincms/admin/tree_editor.py
@@ -393,7 +393,7 @@ class TreeEditor(ExtensionModelAdmin):
         pasted_on = tree_manager.get(pk=request.POST.get('pasted_on'))
         position = request.POST.get('position')
 
-        if position in ('last-child', 'left'):
+        if position in ('last-child', 'left', 'right'):
             try:
                 tree_manager.move_node(cut_item, pasted_on, position)
             except InvalidMove, e:
