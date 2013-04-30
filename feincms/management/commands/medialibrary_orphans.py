af2009e9a1b8cde5338e4171d43644c848173e93
Matthias Kestenholz <mk@spinlock.ch>
2013-04-30 17:45:23 +0200

    Simple helper for finding orphaned files from the media library
    This does not happen with recent versions of FeinCMS anymore. Files of
    deleted media files are automatically unlinked too.

---
+++ feincms/management/commands/medialibrary_orphans.py
@@ -0,0 +1,20 @@
+import os
+
+from django.core.management.base import NoArgsCommand
+from django.utils.encoding import force_unicode
+
+from feincms.module.medialibrary.models import MediaFile
+
+
+class Command(NoArgsCommand):
+    help = "Prints all orphaned files in the `media/medialibrary` folder"
+
+    def handle_noargs(self, **options):
+        mediafiles = list(MediaFile.objects.values_list('file', flat=True))
+
+        # TODO make this smarter, and take MEDIA_ROOT into account
+        for base, dirs, files in os.walk('media/medialibrary'):
+            for f in files:
+                full = os.path.join(base[6:], f)
+                if force_unicode(full) not in mediafiles:
+                    print os.path.join(base, f)
