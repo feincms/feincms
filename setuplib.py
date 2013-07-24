772882c0b338ebce03217d628cf77486a88d2abc
Simon Meers <simon@simonmeers.com>
2013-07-24 23:35:20 +1000

    Make setuplib.py python 2.5 compatible

--- setuplib.py
+++ setuplib.py
@@ -1,5 +1,23 @@
 import os
 
+if not hasattr(os.path, 'relpath'): # python < 2.6
+    # http://www.saltycrane.com/blog/2010/03/ospathrelpath-source-code-python-25/
+    from posixpath import curdir, sep, pardir, join, abspath, commonprefix
+
+    def relpath(path, start=curdir):
+        """Return a relative version of a path"""
+        if not path:
+            raise ValueError("no path specified")
+        start_list = abspath(start).split(sep)
+        path_list = abspath(path).split(sep)
+        # Work out how much of the filepath is shared by start and path.
+        i = len(commonprefix([start_list, path_list]))
+        rel_list = [pardir] * (len(start_list)-i) + path_list[i:]
+        if not rel_list:
+            return curdir
+        return join(*rel_list)
+
+    os.path.relpath = relpath
 
 __all__ = ['find_files']
 
