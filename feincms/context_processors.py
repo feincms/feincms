bfcd64872e22159c22900d47181114a034e0c970
Bojan Mihelac <bmihelac@mihelac.org>
2013-09-30 11:47:49 +0200

    Update context_processors.py
    Remove obsolete documentation.

--- feincms/context_processors.py
+++ feincms/context_processors.py
@@ -3,9 +3,7 @@ from feincms.module.page.models import Page
 
 def add_page_if_missing(request):
     """
-    If this attribute exists, then a page object has been registered already
-    by some other part of the code. We let it decide which page object it
-    wants to pass into the template
+    Returns ``feincms_page`` for request.
     """
 
     try:
