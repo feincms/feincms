7869932dbaa8c88b87b213a315ce134f7631bd8f
Matthias Kestenholz <mk@spinlock.ch>
2013-10-15 09:44:05 +0200

    Fix #450, fix #452: No more crashes because of tuple/list problems with ModelAdmin extensions

--- feincms/module/page/extensions/relatedpages.py
+++ feincms/module/page/extensions/relatedpages.py
@@ -7,15 +7,14 @@ from django.utils.translation import ugettext_lazy as _
 
 from feincms.module.page.models import Page
 
+
 def register(cls, admin_cls):
     cls.add_to_class('related_pages', models.ManyToManyField(Page, blank=True,
         related_name='%(app_label)s_%(class)s_related',
         null=True, help_text=_('Select pages that should be listed as related content.')))
 
-    try:
-        admin_cls.filter_horizontal.append('related_pages')
-    except AttributeError:
-        admin_cls.filter_horizontal = ['related_pages']
+    admin_cls.filter_horizontal = list(getattr(admin_cls, 'filter_horizontal', ()))
+    admin_cls.filter_horizontal.append('related_pages')
 
     admin_cls.add_extension_options(_('Related pages'), {
         'fields': ('related_pages',),
