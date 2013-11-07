5110a8008728061ad3e57ea403e9d09caaaddd81
Matthias Kestenholz <mk@spinlock.ch>
2013-11-07 10:34:17 +0100

    Django 1.6 has been released, run tests with the release version now

--- tests/tox-update-venvs.sh
+++ tests/tox-update-venvs.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
 (
-    cd .tox/py27-1.6.X
-    bin/pip install -U --editable=git+git://github.com/django/django.git@stable/1.6.x#egg=django-dev
-)
-(
     cd .tox/py27-1.7.X
     bin/pip install -U --editable=git+git://github.com/django/django.git@master#egg=django-dev
-)
-(
-    cd .tox/py33-1.6.X
-    bin/pip install -U --editable=git+git://github.com/django/django.git@stable/1.6.x#egg=django-dev
 )
 (
     cd .tox/py33-1.7.X
