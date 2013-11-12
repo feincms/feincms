75b57f18bf61fa29cb6fd8769d3dd16ee04fe552
Matthias Kestenholz <mk@spinlock.ch>
2013-11-12 12:03:52 +0100

    Remove the automatic addition of unknown fields to the page admin fieldset

--- feincms/module/page/modeladmins.py
+++ feincms/module/page/modeladmins.py
@@ -74,30 +74,6 @@ class PageAdmin(item_editor.ItemEditor, tree_editor.TreeEditor):
 
         super(PageAdmin, self).__init__(model, admin_site)
 
-        # The use of fieldsets makes only fields explicitly listed in there
-        # actually appear in the admin form. However, extensions should not be
-        # aware that there is a fieldsets structure and even less modify it;
-        # we therefore enumerate all of the model's field and forcibly add them
-        # to the last section in the admin. That way, nobody is left behind.
-        from django.contrib.admin.util import flatten_fieldsets
-        present_fields = flatten_fieldsets(self.fieldsets)
-
-        for f in self.model._meta.fields:
-            if (not f.name.startswith('_')
-                    and not f.name in ('id', 'lft', 'rght', 'tree_id', 'level')
-                    and not f.auto_created
-                    and not f.name in present_fields
-                    and f.editable):
-                self.fieldsets[1][1]['fields'].append(f.name)
-                warnings.warn(
-                    'Automatically adding %r to %r.fieldsets. This behavior'
-                    ' is deprecated. Use add_extension_options yourself if'
-                    ' you want fields to appear in the page'
-                    ' administration.' % (f.name, self.__class__),
-                    DeprecationWarning)
-            if not f.editable:
-                self.readonly_fields.append(f.name)
-
     in_navigation_toggle = tree_editor.ajax_editable_boolean('in_navigation', _('in navigation'))
 
     def get_readonly_fields(self, request, obj=None):
