681d279ca291c8edfb82b02c3fd91c77f693d09a
Stefan Reinhard <sr@feinheit.ch>
2014-02-04 16:25:49 +0100

    flake8 clean

--- feincms/content/application/models.py
+++ feincms/content/application/models.py
@@ -211,9 +211,8 @@ class ApplicationContent(models.Model):
                             get_fields(self, *args, **kwargs))
 
                     for k, v in self.custom_fields.items():
+                        v.initial = self.instance.parameters.get(k)
                         self.fields[k] = v
-                        if k in self.instance.parameters:
-                            self.fields[k].initial = self.instance.parameters[k]
 
             def save(self, commit=True, *args, **kwargs):
                 # Django ModelForms return the model instance from save. We'll
