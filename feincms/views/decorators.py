83922e85a59d60b86c03baec5425b8e88c5e9607
Matthias Kestenholz <mk@spinlock.ch>
2014-10-10 16:31:47 +0200

    Restore those decorators

--- feincms/views/decorators.py
+++ feincms/views/decorators.py
@@ -1,12 +1,35 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 
-import warnings
+from functools import wraps
 
-from feincms.applications import standalone, unpack
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
-__all__ = ('standalone', 'unpack')
+
+def standalone(view_func):
+    """
+    Marks the view method as standalone view; this means that
+    ``HttpResponse`` objects returned from ``ApplicationContent``
+    are returned directly, without further processing.
+    """
+
+    def inner(request, *args, **kwargs):
+        response = view_func(request, *args, **kwargs)
+        if isinstance(response, HttpResponse):
+            response.standalone = True
+        return response
+    return wraps(view_func)(inner)
 
 
-warnings.warn(
-    'Import @standalone and @unpack from feincms.applications.',
-    DeprecationWarning, stacklevel=2)
+def unpack(view_func):
+    """
+    Marks the returned response as to-be-unpacked if it is a
+    ``TemplateResponse``.
+    """
+
+    def inner(request, *args, **kwargs):
+        response = view_func(request, *args, **kwargs)
+        if isinstance(response, TemplateResponse):
+            response._feincms_unpack = True
+        return response
+    return wraps(view_func)(inner)
