8884ba33cf6494525e271d874efbeafa6881be2e
Matthias Kestenholz <mk@spinlock.ch>
2015-08-19 09:45:17 +0200

    What does not exist cannot be imported

--- feincms/content/richtext/models.py
+++ feincms/content/richtext/models.py
@@ -5,6 +5,6 @@ warnings.warn(
     'Import RichTextContent from feincms.contents instead.',
     DeprecationWarning, stacklevel=2)
 
-__all__ = ('RichTextContent', 'RichTextContentAdminForm')
+__all__ = ('RichTextContent',)
 
-from feincms.contents import RichTextContent, RichTextContentAdminForm
+from feincms.contents import RichTextContent
