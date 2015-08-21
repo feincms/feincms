5a7a8d7560c076498b9b6bea85dd74f0fdd96491
Matthias Kestenholz <mk@spinlock.ch>
2015-08-21 11:49:06 +0200

    Add built-in support for django-filer

--- feincms/contents.py
+++ feincms/contents.py
@@ -148,3 +148,95 @@ class RichTextContent(models.Model):
 
         if cleanse:
             cls.cleanse = to_instance_method(cleanse)
+
+
+try:
+    from filer.fields.file import FilerFileField
+    from filer.fields.image import FilerImageField
+except ImportError:
+    pass
+else:
+
+    class MediaFileContentInline(FeinCMSInline):
+        radio_fields = {'type': admin.VERTICAL}
+
+    class ContentWithFilerFile(models.Model):
+        """
+        MediaFile Content for use with Django-Filer.
+        """
+        feincms_item_editor_inline = MediaFileContentInline
+
+        class Meta:
+            abstract = True
+
+        def render(self, **kwargs):
+            ctx = {'content': self}
+            ctx.update(kwargs)
+            return render_to_string([
+                'content/filer/%s_%s.html' % (self.file_type, self.type),
+                'content/filer/%s.html' % self.type,
+                'content/filer/%s.html' % self.file_type,
+                'content/filer/default.html',
+            ], ctx, context_instance=kwargs.get('context'))
+
+    class FileContent(ContentWithFilerFile):
+        mediafile = FilerFileField(verbose_name=_('file'), related_name='+')
+        file_type = 'file'
+        type = 'download'
+
+        class Meta:
+            abstract = True
+            verbose_name = _('file')
+            verbose_name_plural = _('files')
+
+    class ImageContent(ContentWithFilerFile):
+        """
+        Create a media file content as follows::
+
+            from feincms.content.medialibrary.v2 import MediaFileContent
+            Page.create_content_type(MediaFileContent, TYPE_CHOICES=(
+                ('inline', _('Default')),
+                ('lightbox', _('Lightbox')),
+                ('whatever', _('Whatever')),
+                ))
+
+        For a media file of type 'image' and type 'lightbox', the following
+        templates are tried in order:
+
+        * content/mediafile/image_lightbox.html
+        * content/mediafile/lightbox.html
+        * content/mediafile/image.html
+        * content/mediafile/default.html
+
+        The context contains ``content`` and ``request`` (if available).
+
+        The content.mediafile attribute are as follows (selection):
+        label, description, default_caption, default_alt_text,
+        author, must_always_publish_author_credit,
+        must_always_publish_copyright, date_taken, file, id, is_public, url
+        """
+
+        mediafile = FilerImageField(verbose_name=_('image'), related_name='+')
+        file_type = 'image'
+
+        class Meta:
+            abstract = True
+            verbose_name = _('image')
+            verbose_name_plural = _('images')
+
+        @classmethod
+        def initialize_type(cls, TYPE_CHOICES=None):
+            if TYPE_CHOICES is None:
+                raise ImproperlyConfigured(
+                    'You have to set TYPE_CHOICES when'
+                    ' creating a %s' % cls.__name__)
+
+            cls.add_to_class(
+                'type',
+                models.CharField(
+                    _('type'),
+                    max_length=20,
+                    choices=TYPE_CHOICES,
+                    default=TYPE_CHOICES[0][0],
+                ),
+            )
---
+++ feincms/management/commands/medialibrary_to_filer.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.core.files import File as DjangoFile
+from django.core.management.base import NoArgsCommand
+from django.contrib.auth.models import User
+
+from feincms.contents import MediaFileContent, FileContent, ImageContent
+from feincms.module.medialibrary.models import MediaFile
+from feincms.module.page.models import Page
+
+from filer.models import File, Image
+
+
+PageMediaFileContent = Page.content_type_for(MediaFileContent)
+PageFileContent = Page.content_type_for(FileContent)
+PageImageContent = Page.content_type_for(ImageContent)
+
+
+assert (PageMediaFileContent and PageFileContent and PageImageContent),\
+    'Not all required models available'
+
+
+class Command(NoArgsCommand):
+    help = "Migrate the medialibrary and contents to django-filer"
+
+    def handle_noargs(self, **options):
+        user = User.objects.order_by('pk')[0]
+
+        count = MediaFile.objects.count()
+
+        for i, mediafile in enumerate(MediaFile.objects.order_by('pk')):
+            model = Image if mediafile.type == 'image' else File
+            content_model = PageImageContent if mediafile.type == 'image' else PageFileContent  # noqa
+
+            filerfile = model.objects.create(
+                owner=user,
+                original_filename=mediafile.file.name,
+                file=DjangoFile(
+                    mediafile.file.file,
+                    name=mediafile.file.name,
+                ),
+            )
+
+            contents = PageMediaFileContent.objects.filter(mediafile=mediafile)
+
+            for content in contents:
+                content_model.objects.create(
+                    parent=content.parent,
+                    region=content.region,
+                    ordering=content.ordering,
+                    type=content.type,
+                    mediafile=filerfile,
+                )
+
+                content.delete()
+
+            if not i % 10:
+                self.stdout.write('%s / %s files\n' % (i, count))
