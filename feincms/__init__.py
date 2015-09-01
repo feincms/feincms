0b4bbe4c6f8555349238bb8da1d5099314799e03
Matthias Kestenholz <mk@spinlock.ch>
2015-09-01 12:17:19 +0200

    FeinCMS 2.0a2

--- feincms/__init__.py
+++ feincms/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-VERSION = (2, 0, 'a', 1)
+VERSION = (2, 0, 'a', 2)
 # __version__ = '.'.join(map(str, VERSION))
-__version__ = '2.0a1'
+__version__ = '2.0a2'
 
 
 class LazySettings(object):
