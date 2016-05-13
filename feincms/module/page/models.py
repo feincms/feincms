4a1457c228cdcdd3009ea124d575845bc749f8e9
Joshua Jonah <joshuajonahcom@gmail.com>
2016-05-13 16:38:53 -0400

    Removed trace, works with caching.

--- feincms/module/page/models.py
+++ feincms/module/page/models.py
@@ -74,12 +74,9 @@ class BasePageManager(ActiveAwareContentManagerMixin, TreeManager):
 
         paths = ['/']
         path = path.strip('/')
-        print(path)
         for prefix in settings.FEINCMS_ALLOW_EXTRA_PATH_PREFIX:
             if path.startswith('%s/' % prefix):
                 path = path[len(prefix)+1:]
-        print(path)
-
 
         if path:
             tokens = path.split('/')
