5cd3b53f677fd6ab6e77bee5b7d42cf2ac85e47f
Matthias Kestenholz <mk@feinheit.ch>
2020-10-01 21:00:36 +0200

    Add a workaround for the AppConfig autodiscovery crashes with Django 3.2

--- feincms/apps.py
+++ feincms/apps.py
@@ -1,3 +1,17 @@
-# flake8: noqa
+def __getattr__(key):
+    # Work around Django 3.2's autoloading of *.apps modules (AppConfig
+    # autodiscovery)
+    if key in {
+        "ApplicationContent",
+        "app_reverse",
+        "app_reverse_lazy",
+        "permalink",
+        "UnpackTemplateResponse",
+        "standalone",
+        "unpack",
+    }:
+        from feincms.content.application import models
 
-from feincms.content.application.models import *
+        return getattr(models, key)
+
+    raise AttributeError("Unknown attribute '%s'" % key)
