43bc0201cb67053824a0b0c67d98eb2850c28c30
Martin J. Laubach <mjl@laubach.at>
2022-03-15 16:10:07 +0100

    Make medialibrary_orphans work again

--- feincms/management/commands/medialibrary_orphans.py
+++ feincms/management/commands/medialibrary_orphans.py
@@ -1,20 +1,24 @@
 import os
 
-from django.core.management.base import NoArgsCommand
-from django.utils.encoding import force_str
+from django.conf import settings
+from django.core.management.base import BaseCommand
 
 from feincms.module.medialibrary.models import MediaFile
 
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
     help = "Prints all orphaned files in the `media/medialibrary` folder"
 
-    def handle_noargs(self, **options):
+    def handle(self, **options):
         mediafiles = list(MediaFile.objects.values_list("file", flat=True))
 
-        # TODO make this smarter, and take MEDIA_ROOT into account
-        for base, dirs, files in os.walk("media/medialibrary"):
+        root_len = len(settings.MEDIA_ROOT)
+        medialib_path = os.path.join(settings.MEDIA_ROOT, "medialibrary")
+
+        for base, dirs, files in os.walk(medialib_path):
             for f in files:
-                full = os.path.join(base[6:], f)
-                if force_str(full) not in mediafiles:
-                    self.stdout.write(os.path.join(base, f))
+                if base.startswith(settings.MEDIA_ROOT):
+                    base = base[root_len:]
+                full = os.path.join(base, f)
+                if full not in mediafiles:
+                    self.stdout.write(full)
